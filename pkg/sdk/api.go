326984e1d5a8931009253ef3bd4a448fdc1a3beb
hasbro17 <hasbro17@gmail.com>
2018-02-21 10:49:03 -0800

    *: create dynamic client for watching a resource

--- pkg/sdk/api.go
+++ pkg/sdk/api.go
@@ -3,12 +3,9 @@ package sdk
 import (
 	"context"
 
+	"github.com/coreos/operator-sdk/pkg/k8sclient"
 	sdkHandler "github.com/coreos/operator-sdk/pkg/sdk/handler"
 	sdkInformer "github.com/coreos/operator-sdk/pkg/sdk/informer"
-
-	"github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/rest"
 )
 
 var (
@@ -17,13 +14,18 @@ var (
 )
 
 // Watch watches for changes on the given resource.
-// obj is an instance of the resource type, e.g. &Pod{}.
-// resourcePluralName is the plural name of the resource, e.g. “pods”.
-// resourceClient is the rest client for the resource, e.g. `kubeclient.CoreV1().RESTClient()`.
-// opts provide more options for doing the watch.
+// apiVersion for a resource is of the format "Group/Version" except for the "Core" group whose APIVersion is just "v1". For e.g:
+//   - Deployments have Group "apps" and Version "v1beta2" giving the APIVersion "apps/v1beta2"
+//   - Pods have Group "Core" and Version "v1" giving the APIVersion "v1"
+//   - The custom resource Memcached might have Group "cache.example.com" and Version "v1alpha1" giving the APIVersion "cache.example.com/v1alpha1"
+// kind is the Kind of the resource, e.g "Pod" for pods
+// Consult the API reference for the Group, Version and Kind of a resource: https://kubernetes.io/docs/reference/
+// namespace is the Namespace to watch for the resource
 // TODO: support opts for specifying label selector
-func Watch(resourcePluralName, namespace string, obj runtime.Object, resourceClient rest.Interface) {
-	informer := sdkInformer.New(resourcePluralName, namespace, obj, resourceClient)
+func Watch(apiVersion, kind, namespace string) {
+	// TODO: Error handling for watch failure
+	resourceClient, resourcePluralName, _ := k8sclient.GetResourceClient(apiVersion, kind, namespace)
+	informer := sdkInformer.New(resourcePluralName, namespace, resourceClient)
 	informers = append(informers, informer)
 }
 
@@ -36,11 +38,7 @@ func Handle(handler sdkHandler.Handler) {
 // Run starts the process of Watching resources, handling Events, and processing Actions
 func Run(ctx context.Context) {
 	for _, informer := range informers {
-		err := informer.Run(ctx)
-		if err != nil {
-			logrus.Errorf("failed to run informer: %v", err)
-			return
-		}
+		go informer.Run(ctx)
 	}
 	<-ctx.Done()
 }
