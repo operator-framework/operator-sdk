ca3469c468c6cecc2a2a324ffa1af0d2709b266f
fanmin shi <fanmin.shi@coreos.com>
2018-02-22 15:35:42 -0800

    pkg:generator: add codegen templates

---
+++ pkg/generator/codegen_tmpls.go
@@ -0,0 +1,31 @@
+package generator
+
+const boilerplateTmpl = `/*
+Copyright YEAR The {{.ProjectName}} Authors
+
+Commercial software license.
+*/
+`
+
+const updateGeneratedTmpl = `#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+DOCKER_REPO_ROOT="/go/src/{{.RepoPath}}"
+IMAGE=${IMAGE:-"gcr.io/coreos-k8s-scale-testing/codegen"}
+
+docker run --rm \
+  -v "$PWD":"$DOCKER_REPO_ROOT" \
+  -w "$DOCKER_REPO_ROOT" \
+  "$IMAGE" \
+  "/go/src/k8s.io/code-generator/generate-groups.sh"  \
+  "all" \
+  "{{.RepoPath}}/pkg/generated" \
+  "{{.RepoPath}}/pkg/apis" \
+  "{{.APIDirName}}:{{.Version}}" \
+  --go-header-file "./hack/codegen/boilerplate.go.txt" \
+  $@
+.
+`
