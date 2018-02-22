12ded0d530a349e4b78c756f1936090ce2745aa3
fanmin shi <fanmin.shi@coreos.com>
2018-02-22 13:36:09 -0800

    pkg/generator: "apis/<version>/" to "apis/<apiDirName>/<version>/"

--- pkg/generator/gen_api_doc.go
+++ pkg/generator/gen_api_doc.go
@@ -5,16 +5,16 @@ import (
 	"text/template"
 )
 
-// Doc contains all the customized data needed to generate apis/<version>/doc.go for a new operator
+// Doc contains all the customized data needed to generate apis/<apiDirName>/<version>/doc.go for a new operator
 // when pairing with apisDocTmpl template.
 type Doc struct {
 	GroupName string
 	Version   string
 }
 
-// renderAPIDocFile generates the apis/<version>/doc.go file.
+// renderAPIDocFile generates the apis/<apiDirName>/<version>/doc.go file.
 func renderAPIDocFile(w io.Writer, groupName, version string) error {
-	t := template.New("apis/<version>/doc.go")
+	t := template.New("apis/<apiDirName>/<version>/doc.go")
 	t, err := t.Parse(apiDocTmpl)
 	if err != nil {
 		return err
--- pkg/generator/gen_api_types.go
+++ pkg/generator/gen_api_types.go
@@ -5,16 +5,16 @@ import (
 	"text/template"
 )
 
-// Types contains all the customized data needed to generate apis/<version>/types.go
+// Types contains all the customized data needed to generate apis/<apiDirName>/<version>/types.go
 // for a new operator when pairing with apisTypesTmpl template.
 type Types struct {
 	Version string
 	Kind    string
 }
 
-// renderAPITypesFile generates the apis/<version>/types.go file.
+// renderAPITypesFile generates the apis/<apiDirName>/<version>/types.go file.
 func renderAPITypesFile(w io.Writer, kind, version string) error {
-	t := template.New("apis/<version>/types.go")
+	t := template.New("apis/<apiDirName>/<version>/types.go")
 	t, err := t.Parse(apiTypesTmpl)
 	if err != nil {
 		return err
