544a28cc24be61ac61ef66d22881a8bab7a071c4
Hongchao Deng <hongchaodeng@users.noreply.github.com>
2018-02-22 14:10:24 -0800

    generator: update main() to use latest Watch() (#42)

--- pkg/generator/gen_main.go
+++ pkg/generator/gen_main.go
@@ -16,39 +16,11 @@ const (
 type Main struct {
 	// imports
 	OperatorSDKImport string
-	APIImport         string
 	StubImport        string
-
-	// Service defintion in pkg/apis/..
-	ServicePlural string
-	Service       string
 }
 
-// renderMainFile generates the cmd/<projectName>/main.go file given a repo path ("github.com/coreos/play"), apiVersion ("v1alpha1"),
-// api dir name ("play"), service ("PlayService"), and servicePlural ("PlayServicePlural").
-//
-// for example:
-// renderMain(w, "github.com/coreos/play", "v1alpha1", "play", "PlayService", "PlayServicePlural" )
-// output:
-//
-// package main
-//
-// import (
-// 	"context"
-//
-// 	sdk "github.com/coreos/operator-sdk/pkg/sdk"
-// 	api "github.com/coreos/play/pkg/apis/play/v1alpha1"
-// 	stub "github.com/coreos/play/pkg/stub"
-// )
-//
-// func main() {
-// 	namespace := "default"
-// 	sdk.Watch(api.PlayServicePlural, namespace, api.PlayService)
-//  	sdk.Handle(&stub.Handler{})
-//  	sdk.Run(context.TODO())
-// }
-//
-func renderMainFile(w io.Writer, repo, version, apiDirName, service, servicePlural string) error {
+// renderMainFile generates the cmd/<projectName>/main.go file given a repo path ("github.com/coreos/play")
+func renderMainFile(w io.Writer, repo string) error {
 	t := template.New("cmd/<projectName>/main.go")
 	t, err := t.Parse(mainTmpl)
 	if err != nil {
@@ -57,10 +29,7 @@ func renderMainFile(w io.Writer, repo, version, apiDirName, service, servicePlur
 
 	m := Main{
 		OperatorSDKImport: sdkImport,
-		APIImport:         filepath.Join(repo, apisDir, apiDirName, version),
 		StubImport:        filepath.Join(repo, stubDir),
-		ServicePlural:     servicePlural,
-		Service:           service,
 	}
 	return t.Execute(w, m)
 }
