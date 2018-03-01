bfa0be714634c6c67cba2d76c488e1a286189469
fanmin shi <fanmin.shi@coreos.com>
2018-03-01 11:09:45 -0800

    cmd: implemnt k8s command

--- commands/operator-sdk/cmd/generate/k8s.go
+++ commands/operator-sdk/cmd/generate/k8s.go
@@ -1,10 +1,18 @@
 package generate
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"os/exec"
 
 	cmdError "github.com/coreos/operator-sdk/commands/operator-sdk/error"
+
 	"github.com/spf13/cobra"
+)
+
+const (
+	k8sGenerated = "./tmp/codegen/update-generated.sh"
 )
 
 func NewGenerateK8SCmd() *cobra.Command {
@@ -20,6 +28,13 @@ to comply with kube-API requirements.
 
 func k8sFunc(cmd *cobra.Command, args []string) {
 	if len(args) != 0 {
-		cmdError.ExitWithError(cmdError.ExitBadArgs, fmt.Errorf("k8s command doesn't accept any inputs."))
+		cmdError.ExitWithError(cmdError.ExitBadArgs, errors.New("k8s command doesn't accept any arguments."))
 	}
+
+	kcmd := exec.Command(k8sGenerated)
+	o, err := kcmd.CombinedOutput()
+	if err != nil {
+		cmdError.ExitWithError(cmdError.ExitError, fmt.Errorf("failed to perform code-generation for CustomResources: (%v)", string(o)))
+	}
+	fmt.Fprintln(os.Stdout, string(o))
 }
