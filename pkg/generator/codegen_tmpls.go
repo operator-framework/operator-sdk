feb44eb4c0e43f10b877a41adc54c1867a46e040
Eric Paris <eparis@redhat.com>
2018-05-03 16:20:06 -0400

    pkg/generator: Give docker access to GOPATH if SELinux is enforcing
    The code generator container was attempting to write into my GOPATH which is
    labeled as part of my home directory, and thus docker containers are not, by
    default, given access. This patch mountes the volume with `:Z` which will
    cause docker to relabel the volume granting access to the docker container.
    
    Fixes #213

--- pkg/generator/codegen_tmpls.go
+++ pkg/generator/codegen_tmpls.go
@@ -27,7 +27,7 @@ DOCKER_REPO_ROOT="/go/src/{{.RepoPath}}"
 IMAGE=${IMAGE:-"gcr.io/coreos-k8s-scale-testing/codegen:1.9.3"}
 
 docker run --rm \
-  -v "$PWD":"$DOCKER_REPO_ROOT" \
+  -v "$PWD":"$DOCKER_REPO_ROOT":Z \
   -w "$DOCKER_REPO_ROOT" \
   "$IMAGE" \
   "/go/src/k8s.io/code-generator/generate-groups.sh"  \
--- pkg/generator/generator_test.go
+++ pkg/generator/generator_test.go
@@ -311,7 +311,7 @@ DOCKER_REPO_ROOT="/go/src/github.com/example-inc/app-operator"
 IMAGE=${IMAGE:-"gcr.io/coreos-k8s-scale-testing/codegen:1.9.3"}
 
 docker run --rm \
-  -v "$PWD":"$DOCKER_REPO_ROOT" \
+  -v "$PWD":"$DOCKER_REPO_ROOT":Z \
   -w "$DOCKER_REPO_ROOT" \
   "$IMAGE" \
   "/go/src/k8s.io/code-generator/generate-groups.sh"  \
