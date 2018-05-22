b5c88e4ca7cccf5f3f1f10345418d50a212024f9
jhernandezb <contact@jhernandez.me>
2018-05-22 15:09:14 -0600

    commands/operator-sdk/cmd/local: add namespace flag

--- commands/operator-sdk/cmd/up/local.go
+++ commands/operator-sdk/cmd/up/local.go
@@ -28,6 +28,7 @@ kubernetes cluster using a kubeconfig file.
 
 	upLocalCmd.Flags().StringVar(&kubeConfig, "kubeconfig", "", "The file path to kubernetes configuration file; defaults to $HOME/.kube/config")
 	upLocalCmd.Flags().StringVar(&operatorFlags, "operator-flags", "", "The flags that the operator needs. Example: \"--flag1 value1 --flag2=value2\"")
+	upLocalCmd.Flags().StringVar(&namespace, "namespace", "default", "The namespace where the operator watches for changes.")
 
 	return upLocalCmd
 }
@@ -35,6 +36,7 @@ kubernetes cluster using a kubeconfig file.
 var (
 	kubeConfig    string
 	operatorFlags string
+	namespace     string
 )
 
 const (
@@ -78,7 +80,7 @@ func upLocal(projectName string) {
 	dc := exec.Command(gocmd, args...)
 	dc.Stdout = os.Stdout
 	dc.Stderr = os.Stderr
-	dc.Env = append(os.Environ(), fmt.Sprintf("%v=%v", k8sutil.KubeConfigEnvVar, kubeConfig))
+	dc.Env = append(os.Environ(), fmt.Sprintf("%v=%v", k8sutil.KubeConfigEnvVar, kubeConfig), fmt.Sprintf("%v=%v", k8sutil.WatchNamespaceEnvVar, namespace))
 	err := dc.Run()
 	if err != nil {
 		cmdError.ExitWithError(cmdError.ExitError, fmt.Errorf("failed to run operator locally: %v", err))
