2861184c9c2b2c94670622a6d34b0c266c7ecb0e
Ish Shah <ishah@redhat.com>
2018-06-05 16:55:30 -0700

    Added missing functions

---
+++ pkg/generator/gen_version.go
@@ -0,0 +1,42 @@
+// Copyright 2018 The Operator-SDK Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package generator
+
+import (
+	"io"
+	"text/template"
+)
+
+// Version contains the verison number string and gitsha string
+type Version struct {
+	// imports
+	VersionNumber string
+	GitSHA        string
+}
+
+// renderVersionFile generates the version/version.go file.
+func renderVersionFile(w io.Writer, versionNumber string, gitSha string) error {
+	t := template.New("version/version.go")
+	t, err := t.Parse(versionTmpl)
+	if err != nil {
+		return err
+	}
+
+	v := Version{
+		VersionNumber: versionNumber,
+		GitSHA:        gitSha,
+	}
+	return t.Execute(w, v)
+}
