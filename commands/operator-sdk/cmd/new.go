5aa47d2c4d4dba2523611c297fcd0429bac0406a
Ish Shah <ishah@redhat.com>
2018-06-05 15:33:18 -0700

    Updated error message, removed redundant path string

--- commands/operator-sdk/cmd/new.go
+++ commands/operator-sdk/cmd/new.go
@@ -161,11 +161,10 @@ func execCmd(stdout *os.File, cmd string, args ...string) {
 }
 
 func pullDep() {
-	depPath, err := exec.LookPath(dep)
+	_, err := exec.LookPath(dep)
 	if err != nil {
-		cmdError.ExitWithError(cmdError.ExitError, err)
+		cmdError.ExitWithError(cmdError.ExitError, fmt.Errorf("Looking for dep in $PATH: %v", err))
 	}
-	fmt.Printf("dep is available at %s\n", depPath)
 	fmt.Fprintln(os.Stdout, "Run dep ensure ...")
 	execCmd(os.Stdout, dep, ensureCmd, "-v")
 	fmt.Fprintln(os.Stdout, "Run dep ensure done")
