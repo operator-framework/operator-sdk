1f5cadc6e6742761d1c1603a8c66b8d3de034d8d
Ish Shah <ishah@redhat.com>
2018-06-18 14:30:35 -0700

    Added main test, handler and api types failing

--- pkg/generator/generator_test.go
+++ pkg/generator/generator_test.go
@@ -16,6 +16,7 @@ package generator
 
 import (
 	"bytes"
+	"path/filepath"
 	"strings"
 	"testing"
 )
@@ -350,5 +351,62 @@ func TestGenTypes(t *testing.T) {
 	}
 	if typesExp != buf.String() {
 		t.Errorf(errorMessage, typesExp, buf.String())
+	}
+}
+
+const mainExp = `package main
+
+import (
+	"context"
+	"runtime"
+
+	stub "github.com/example-inc/app-operator/pkg/stub"
+	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
+	k8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
+	sdkVersion "github.com/operator-framework/operator-sdk/version"
+
+	"github.com/sirupsen/logrus"
+)
+
+func printVersion() {
+	logrus.Infof("Go Version: %s", runtime.Version())
+	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
+	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
+}
+
+func main() {
+	printVersion()
+
+	resource := "app.example.com/v1alpha1"
+	kind := "AppService"
+	namespace, err := k8sutil.GetWatchNamespace()
+	if err != nil {
+		logrus.Fatalf("Failed to get watch namespace: %v", err)
+	}
+	resyncPeriod := 5
+	logrus.Infof("Watching %s, %s, %s, %d", resource, kind, namespace, resyncPeriod)
+	sdk.Watch(resource, kind, namespace, resyncPeriod)
+	sdk.Handle(stub.NewHandler())
+	sdk.Run(context.TODO())
+}
+`
+
+func TestGenMain(t *testing.T) {
+	buf := &bytes.Buffer{}
+	td := tmplData{
+		OperatorSDKImport: sdkImport,
+		StubImport:        filepath.Join(appRepoPath, stubDir),
+		K8sutilImport:     k8sutilImport,
+		SDKVersionImport:  versionImport,
+		APIVersion:        appAPIVersion,
+		Kind:              appKind,
+	}
+	if err := renderFile(buf, "cmd/<projectName>/main.go", mainTmpl, td); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if mainExp != buf.String() {
+		t.Errorf(errorMessage, mainExp, buf.String())
 	}
 }
