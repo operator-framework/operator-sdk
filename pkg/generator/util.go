8e0bb43c71673c3f11609494f93c3f74a2b61d5b
Ish Shah <ishah@redhat.com>
2018-07-02 16:47:14 -0700

    Added support for multiple word endings

--- pkg/generator/util.go
+++ pkg/generator/util.go
@@ -1,11 +1,23 @@
 package generator
 
 // toPlural makes "input" word plural.
-// TODO: make this more grammatically correct for special nouns.
+// TODO: make this an input parameter as English grammar is highly variable
 func toPlural(input string) string {
 	lastchar := input[len(input)-1:]
 
 	if lastchar == "s" {
+		return input + "es"
+	} else if lastchar == "x" {
+		return input + "es"
+	} else if lastchar == "y" {
+		return input[0:len(input)-2] + "ies"
+	}
+
+	lasttwo := input[len(input)-2:]
+
+	if lasttwo == "ch" {
+		return input + "es"
+	} else if lasttwo == "sh" {
 		return input + "es"
 	}
 
