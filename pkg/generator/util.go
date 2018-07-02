35d9069eac2d564102c84dc47745fcba60c00a13
Ish Shah <ishah@redhat.com>
2018-07-02 16:37:17 -0700

    Add support for ending in s

--- pkg/generator/util.go
+++ pkg/generator/util.go
@@ -3,5 +3,11 @@ package generator
 // toPlural makes "input" word plural.
 // TODO: make this more grammatically correct for special nouns.
 func toPlural(input string) string {
+	lastchar := input[len(input)-1:]
+
+	if lastchar == "s" {
+		return input + "es"
+	}
+
 	return input + "s"
 }
