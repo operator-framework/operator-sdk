5756072a02181d993fc01289005294c8de25ae82
Alexander Pavel <apavel@redhat.com>
2018-08-10 12:15:51 -0700

    hack: add check_error_case.sh and test_lib.sh

---
+++ hack/check_error_case.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source "hack/lib/test_lib.sh"
+
+echo "Checking case of error messages..."
+allfiles=$(listFiles)
+output=$(grep -Rn 'Fatalf("[[:upper:]]\|Errorf("[[:upper:]]\|errors.New("[[:upper:]]' $allfiles)
+if [ -n "${output}" ]; then
+  echo "Error messages in wrong case:"
+  echo "${output}"
+  exit 255
+fi
