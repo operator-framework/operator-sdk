7e6987c42505b74be0fcd8f3b2c5d91b2ae98e81
Alex Pavel <alexpavel123@gmail.com>
2018-09-05 12:55:56 -0700

    *: bump version to 0.0.6+git and set template to master (#458)

--- pkg/generator/templates.go
+++ pkg/generator/templates.go
@@ -295,8 +295,8 @@ required = [
 [[constraint]]
   name = "github.com/operator-framework/operator-sdk"
   # The version rule is used for a specific release and the master branch for in between releases.
-  # branch = "master"
-  version = "=v0.0.6"
+  branch = "master"
+  # version = "=v0.0.6"
 `
 
 const projectGitignoreTmpl = `
