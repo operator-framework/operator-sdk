943e9b8078e80ad8c51d04797d692f3f41d8ae3b
Alexander Pavel <apavel@redhat.com>
2018-09-12 11:34:45 -0700

    test/e2e/memcached_test.go: increase up local timeout
    The increased load of the openshift cluster means that up local
    is occasionally still not ready after 80 seconds (minikube was
    close to the timeout as well, but it never seemed to hit the
    timeout). Bumping the timeout to 100 seconds should fix this.

--- test/e2e/memcached_test.go
+++ test/e2e/memcached_test.go
@@ -186,7 +186,7 @@ func MemcachedLocal(t *testing.T) {
 	ctx.AddFinalizerFn(func() error { return cmd.Process.Signal(os.Interrupt) })
 
 	// wait for operator to start (may take a minute to compile the command...)
-	err = wait.Poll(time.Second*5, time.Second*80, func() (done bool, err error) {
+	err = wait.Poll(time.Second*5, time.Second*100, func() (done bool, err error) {
 		file, err := ioutil.ReadFile("stderr.txt")
 		if err != nil {
 			return false, err
@@ -197,7 +197,7 @@ func MemcachedLocal(t *testing.T) {
 		return true, nil
 	})
 	if err != nil {
-		t.Fatalf("local operator not ready after 60 seconds: %v\n", err)
+		t.Fatalf("local operator not ready after 100 seconds: %v\n", err)
 	}
 
 	if err = memcachedScaleTest(t, f, ctx); err != nil {
