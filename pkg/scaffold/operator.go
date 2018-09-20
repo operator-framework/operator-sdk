401cdb3622a6738780e7c6f83b1041c30fe91f22
fanmin shi <fanmin.shi@coreos.com>
2018-09-20 12:33:06 -0700

    *: add codegen for deploy/operator.yaml

---
+++ pkg/scaffold/operator.go
@@ -0,0 +1,77 @@
+// Copyright 2018 The Operator-SDK Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scaffold
+
+import (
+	"io"
+	"text/template"
+)
+
+type operator struct {
+	in *OperatorInput
+}
+
+// OperatorInput is the input needed to generate a pkg/deploy/operator.yaml.
+type OperatorInput struct {
+	// ProjectName is the name of the operator project.
+	ProjectName string
+}
+
+func NewOperatorCodegen(in *OperatorInput) Codegen {
+	return &operator{in: in}
+}
+
+func (r *operator) Render(w io.Writer) error {
+	t := template.New("operator.go")
+	t, err := t.Parse(operatorTemplate)
+	if err != nil {
+		return err
+	}
+
+	return t.Execute(w, r.in)
+}
+
+const operatorTemplate = `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: {{.ProjectName}}
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      name: {{.ProjectName}}
+  template:
+    metadata:
+      labels:
+        name: {{.ProjectName}}
+    spec:
+      containers:
+        - name: {{.ProjectName}}
+          image: REPLACE_IMAGE // replace this with the built image.
+          ports:
+          - containerPort: 60000
+            name: metrics
+          command:
+          - {{.ProjectName}}
+          imagePullPolicy: Always
+          env:
+            - name: WATCH_NAMESPACE
+              valueFrom:
+                fieldRef:
+                  fieldPath: metadata.namespace
+            - name: OPERATOR_NAME
+              value: "{{.ProjectName}}"
+`
