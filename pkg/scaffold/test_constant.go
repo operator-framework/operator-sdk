87542a7fddb5058d234f71c5469cc6425f0ac927
fanmin shi <fanmin.shi@coreos.com>
2018-10-01 14:34:45 -0700

    *: add add_controller_test and addtoscheme_test

---
+++ pkg/scaffold/test_constant.go
@@ -0,0 +1,9 @@
+package scaffold
+
+const (
+	// test constants describing an app operator project
+	appProjectName = "app-operator"
+	appProjectPath = "github.com/example-inc/" + appProjectName
+	appApiVersion  = "app.example.com/v1alpha1"
+	appKind        = "AppService"
+)
