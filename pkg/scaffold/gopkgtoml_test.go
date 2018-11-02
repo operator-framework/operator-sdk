cfee95d135759d39d4e27fbb481b1397706973ea
Alexander Pavel <apavel@redhat.com>
2018-11-02 14:32:09 -0700

    pkg/scaffold,version,changelog: bump to v0.1.1

--- pkg/scaffold/gopkgtoml_test.go
+++ pkg/scaffold/gopkgtoml_test.go
@@ -75,8 +75,8 @@ required = [
 [[constraint]]
   name = "github.com/operator-framework/operator-sdk"
   # The version rule is used for a specific release and the master branch for in between releases.
-  branch = "v0.1.x" #osdk_branch_annotation
-  # version = "=v0.1.0" #osdk_version_annotation
+  # branch = "v0.1.x" #osdk_branch_annotation
+  version = "=v0.1.1" #osdk_version_annotation
 
 [prune]
   go-tests = true
