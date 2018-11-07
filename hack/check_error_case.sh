5eb3e661f94c092a0df8c737607dcff768706031
Eric Stroczynski <ericstroczynski@gmail.com>
2018-11-07 12:28:28 -0800

    hack/check_error_case.sh: check against Error/Fatal

--- hack/check_error_case.sh
+++ hack/check_error_case.sh
@@ -7,7 +7,7 @@ source "hack/lib/test_lib.sh"
 
 echo "Checking case of error messages..."
 allfiles=$(listFiles)
-output=$(grep -Rn 'Fatalf("[[:upper:]]\|Errorf("[[:upper:]]\|errors.New("[[:upper:]]' $allfiles)
+output=$(grep -ERn 'Fatal(f)?\("[[:upper:]]|Error(f)?\("[[:upper:]]|Error\((err|nil), "[[:upper:]]|errors.New\("[[:upper:]]' $allfiles)
 if [ -n "${output}" ]; then
   echo "Error messages in wrong case:"
   echo "${output}"
