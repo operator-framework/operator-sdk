f61a67ce590777bc5db2027f8b390d7ed803c0a2
Lili Cosic <cosiclili@gmail.com>
2018-11-19 14:04:03 +0100

    pkg: Move metrics from sdk pkg
    Since the leftover logic in the pkg/sdk was just metrics related.

--- pkg/metrics/service-monitor.go
+++ pkg/metrics/service-monitor.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sdk
+package metrics
 
 import (
 	monitoringv1 "github.com/coreos/prometheus-operator/pkg/client/monitoring/v1"
