4268f3fe64185cddc8ab9903a5745ed09eed34b7
Alexander Pavel <apavel@redhat.com>
2018-11-19 16:37:39 -0800

    hack,test: add initial tests for scorecard command

---
+++ hack/tests/scorecard-subcommand.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DEST_IMAGE="quay.io/example/scorecard-proxy"
+
+set -ex
+
+# build scorecard-proxy image (and delete intermediate builder image)
+docker build -t scorecard-proxy -f images/scorecard-proxy/Dockerfile .
+
+# the test framework directory has all the manifests needed to run the cluster
+pushd test/test-framework
+operator-sdk scorecard --cr-manifest deploy/crds/cache_v1alpha1_memcached_cr.yaml --basic-tests --init-timeout 60 --olm-tests --csv-path deploy/memcachedoperator.0.0.2.csv.yaml | grep "Total Score: 6/8 points"
+popd
