b3980b90d5c9dcd0714793819d2089cd9a26f90c
Alexander Pavel <apavel@redhat.com>
2018-11-30 11:42:13 -0800

    pkg/scaffold: change branch in gopkg to v0.2.x

--- pkg/scaffold/gopkgtoml.go
+++ pkg/scaffold/gopkgtoml.go
@@ -85,7 +85,7 @@ required = [
 [[constraint]]
   name = "github.com/operator-framework/operator-sdk"
   # The version rule is used for a specific release and the master branch for in between releases.
-  # branch = "master" #osdk_branch_annotation
+  # branch = "v0.2.x" #osdk_branch_annotation
   version = "=v0.2.0" #osdk_version_annotation
 
 [prune]
