50cf5e26c4928d8020a6b2beef902a1861faee7a
Joe Lanford <joe.lanford@gmail.com>
2018-12-03 12:27:24 -0500

    internal/util/diffutil: removing obsolete comment

--- internal/util/diffutil/diff_util.go
+++ internal/util/diffutil/diff_util.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Modified from github.com/kubernetes-sigs/controller-tools/pkg/util/util.go
-
 package diffutil
 
 import (
