d6541603f2219f33966078a087732ecfc7a1dde5
Alexander Pavel <alexpavel123@gmail.com>
2018-12-10 14:27:06 -0800

    version,pkg/scaffold,changelog: update to v0.2.1 (#833)

--- pkg/scaffold/gopkgtoml.go
+++ pkg/scaffold/gopkgtoml.go
@@ -86,7 +86,7 @@ required = [
   name = "github.com/operator-framework/operator-sdk"
   # The version rule is used for a specific release and the master branch for in between releases.
   # branch = "v0.2.x" #osdk_branch_annotation
-  version = "=v0.2.0" #osdk_version_annotation
+  version = "=v0.2.1" #osdk_version_annotation
 
 [prune]
   go-tests = true
