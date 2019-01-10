8ed9b6f44b57172c747c4adf65a8bb2b407076a6
Eric Stroczynski <ericstroczynski@gmail.com>
2019-01-10 14:15:22 -0800

    remove duplicates from grep output

--- hack/check_error_log_msg_format.sh
+++ hack/check_error_log_msg_format.sh
@@ -7,20 +7,17 @@ source "hack/lib/test_lib.sh"
 
 echo "Checking format of error and log messages..."
 allfiles=$(listFiles)
-log_case_output=$(grep -ERn '(Error\((.*[Ee]rr|nil), |[^(fmt\.)]Error(f)?\(|Fatal(f)?\(|Info(f)?\(|Warn(f)?\()"[[:lower:]]' $allfiles)
+log_case_output=$(grep -ERn '(Error\((.*[Ee]rr|nil), |[^(fmt\.)]Error(f)?\(|Fatal(f)?\(|Info(f)?\(|Warn(f)?\()"[[:lower:]]' $allfiles | sort -u)
 if [ -n "${log_case_output}" ]; then
-  echo "Log messages in wrong case:"
-  echo "${log_case_output}"
+  echo -e "Log messages in wrong case:\n${log_case_output}"
 fi
-err_case_output=$(grep -ERn '(errors\.New|fmt\.Errorf)\("[[:upper:]]' $allfiles)
+err_case_output=$(grep -ERn '(errors\.New|fmt\.Errorf)\("[[:upper:]]' $allfiles | sort -u)
 if [ -n "${err_case_output}" ]; then
-  echo "Error messages in wrong case:"
-  echo "${err_case_output}"
+  echo -e "Error messages in wrong case:\n${err_case_output}"
 fi
-err_punct_output=$(grep -ERn '(errors\.New|fmt\.Errorf)\(".*\."' $allfiles)
+err_punct_output=$(grep -ERn '(errors\.New|fmt\.Errorf)\(".*\."' $allfiles | sort -u)
 if [ -n "${err_punct_output}" ]; then
-  echo "Error messages have ending punctuation:"
-  echo "${err_punct_output}"
+  echo -e "Error messages have ending punctuation:\n${err_punct_output}"
 fi
 
 if [[ -n "$log_case_output" || -n "$err_case_output" || -n "$err_punct_output" ]]; then
