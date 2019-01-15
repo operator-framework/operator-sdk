fbb0c215bf386ef5f7ac11ebc0ef320291b7cc93
Lili Cosic <cosiclili@gmail.com>
2019-01-15 13:56:05 +0100

    hack/ci: Add check for valid commit range
    In cases when PRs were force pushed and the commit range was not
    valid, the PR job would still pass as we did catch the error. This adds
    a check and exits in case of an invalid commit range.

--- hack/ci/check-doc-only-update.sh
+++ hack/ci/check-doc-only-update.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_COMMIT_RANGE" != "" ] && ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md)|(\.MD)|(\.png)|(\.pdf)|^(doc/)|^(MAINTAINERS)|^(LICENSE)'; then
-  echo "Only doc files were updated, not running the CI."
-  exit
+set -ex
+
+# Make sure the TRAVIS_COMMIT_RANGE is valid, by catching any errors and exiting.
+if [ -z "$TRAVIS_COMMIT_RANGE" ] || ! git rev-list --quiet $TRAVIS_COMMIT_RANGE; then
+  echo "Failed to check the commit range is valid."
+  return 0
 fi
 
+if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md)|(\.MD)|(\.png)|(\.pdf)|^(doc/)|^(MAINTAINERS)|^(LICENSE)'; then
+  echo "Only doc files were updated, not running the CI."
+  exit 0
+fi
