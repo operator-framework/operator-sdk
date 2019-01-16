3edec898fa6739a87cc1df1e75339f77a67b0070
Alexander Pavel <apavel@redhat.com>
2019-01-16 10:09:30 -0800

    hack/tests/scorecard: reduce duplicate debug info

--- hack/tests/scorecard-subcommand.sh
+++ hack/tests/scorecard-subcommand.sh
@@ -2,7 +2,7 @@
 
 DEST_IMAGE="quay.io/example/scorecard-proxy"
 
-set -ex
+set -e
 
 # build scorecard-proxy image (and delete intermediate builder image)
 ./hack/image/build-scorecard-proxy-image.sh $DEST_IMAGE
@@ -10,6 +10,5 @@ set -ex
 # the test framework directory has all the manifests needed to run the cluster
 pushd test/test-framework
 commandoutput="$(operator-sdk scorecard --cr-manifest deploy/crds/cache_v1alpha1_memcached_cr.yaml --init-timeout 60 --csv-path deploy/memcachedoperator.0.0.2.csv.yaml --verbose --proxy-image $DEST_IMAGE --proxy-pull-policy Never 2>&1)"
-echo $commandoutput
 echo $commandoutput | grep "Total Score: 6/8 points"
 popd
