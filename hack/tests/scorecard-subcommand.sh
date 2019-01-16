c1d4cf1b1bcb3518e7f6cd037f4ded97d6ccf1b6
Alexander Pavel <apavel@redhat.com>
2019-01-16 09:51:33 -0800

    hack/tests/scorecard: use env var for image name

--- hack/tests/scorecard-subcommand.sh
+++ hack/tests/scorecard-subcommand.sh
@@ -5,11 +5,11 @@ DEST_IMAGE="quay.io/example/scorecard-proxy"
 set -ex
 
 # build scorecard-proxy image (and delete intermediate builder image)
-./hack/image/build-scorecard-proxy-image.sh scorecard-proxy
+./hack/image/build-scorecard-proxy-image.sh $DEST_IMAGE
 
 # the test framework directory has all the manifests needed to run the cluster
 pushd test/test-framework
-commandoutput="$(operator-sdk scorecard --cr-manifest deploy/crds/cache_v1alpha1_memcached_cr.yaml --init-timeout 60 --csv-path deploy/memcachedoperator.0.0.2.csv.yaml --verbose --proxy-image scorecard-proxy --proxy-pull-policy Never 2>&1)"
+commandoutput="$(operator-sdk scorecard --cr-manifest deploy/crds/cache_v1alpha1_memcached_cr.yaml --init-timeout 60 --csv-path deploy/memcachedoperator.0.0.2.csv.yaml --verbose --proxy-image $DEST_IMAGE --proxy-pull-policy Never 2>&1)"
 echo $commandoutput
 echo $commandoutput | grep "Total Score: 6/8 points"
 popd
