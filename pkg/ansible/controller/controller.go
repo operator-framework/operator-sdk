9a8b5598fbd550b9f2d7b257cb62b37f2687936f
Dylan Murray <dymurray@redhat.com>
2019-01-23 12:25:21 -0500

    Add CI tests

--- pkg/ansible/controller/controller.go
+++ pkg/ansible/controller/controller.go
@@ -26,6 +26,7 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	crthandler "sigs.k8s.io/controller-runtime/pkg/handler"
@@ -66,13 +67,16 @@ func Add(mgr manager.Manager, options Options) *controller.Controller {
 
 	scheme := mgr.GetScheme()
 	_, err := scheme.New(options.GVK)
-	if err != nil {
+	if runtime.IsNotRegisteredError(err) {
 		// Register the GVK with the schema
 		scheme.AddKnownTypeWithName(options.GVK, &unstructured.Unstructured{})
 		metav1.AddToGroupVersion(mgr.GetScheme(), schema.GroupVersion{
 			Group:   options.GVK.Group,
 			Version: options.GVK.Version,
 		})
+	} else if err != nil {
+		log.Error(err, "")
+		os.Exit(1)
 	}
 
 	//Create new controller runtime controller and set the controller to watch GVK.
