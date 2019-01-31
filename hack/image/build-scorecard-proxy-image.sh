767697a0b541514c563a47bca59a43797f328a75
Alexander Pavel <apavel@redhat.com>
2019-01-31 12:14:52 -0800

    hack/image: build scorecard proxy with trimpath

--- hack/image/build-scorecard-proxy-image.sh
+++ hack/image/build-scorecard-proxy-image.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # build operator binary and base image
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o images/scorecard-proxy/scorecard-proxy images/scorecard-proxy/cmd/proxy/main.go
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -gcflags "all=-trimpath=${GOPATH}" -asmflags "all=-trimpath=${GOPATH}" -o images/scorecard-proxy/scorecard-proxy images/scorecard-proxy/cmd/proxy/main.go
 pushd images/scorecard-proxy
 docker build -t "$1" .
 popd
