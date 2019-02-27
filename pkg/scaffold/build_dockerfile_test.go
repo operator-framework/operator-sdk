6349fa68cd37169ce83a5dd9238261c2beca037e
Joe Lanford <joe.lanford@gmail.com>
2019-02-27 12:15:27 -0500

    pkg/scaffold,images: use registry.access.redhat.com/ubi7-dev-preview/ubi-minimal:7.6 for base image

--- pkg/scaffold/build_dockerfile_test.go
+++ pkg/scaffold/build_dockerfile_test.go
@@ -33,7 +33,7 @@ func TestDockerfile(t *testing.T) {
 	}
 }
 
-const dockerfileExp = `FROM registry.access.redhat.com/ubi7/ubi-minimal:7.6
+const dockerfileExp = `FROM registry.access.redhat.com/ubi7-dev-preview/ubi-minimal:7.6
 
 ENV OPERATOR=/usr/local/bin/app-operator \
     USER_UID=1001 \
