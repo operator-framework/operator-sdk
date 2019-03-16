20e36fe63adcfc624870beef29da2b399ef74bcf
Ish Shah <ishah@redhat.com>
2019-03-15 17:00:19 -0700

    post release versioning

--- pkg/scaffold/gopkgtoml_test.go
+++ pkg/scaffold/gopkgtoml_test.go
@@ -94,8 +94,8 @@ required = [
 [[constraint]]
   name = "github.com/operator-framework/operator-sdk"
   # The version rule is used for a specific release and the master branch for in between releases.
-  # branch = "master" #osdk_branch_annotation
-  version = "=v0.6.0" #osdk_version_annotation
+  branch = "master" #osdk_branch_annotation
+  # version = "=v0.6.0" #osdk_version_annotation
 
 [prune]
   go-tests = true
