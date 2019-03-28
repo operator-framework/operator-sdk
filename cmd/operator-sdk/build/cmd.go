e1f80b61ac3a62c1b7c4a04f732364c5ab09ab56
Eric Stroczynski <ericstroczynski@gmail.com>
2019-03-28 13:54:38 -0700

    *: replace 'go build' with projutil.GoBuild()

--- cmd/operator-sdk/build/cmd.go
+++ cmd/operator-sdk/build/cmd.go
@@ -147,7 +147,6 @@ func buildFunc(cmd *cobra.Command, args []string) error {
 	}
 
 	projutil.MustInProjectRoot()
-	goBuildEnv := append(os.Environ(), "GOOS=linux", "GOARCH=amd64", "CGO_ENABLED=0")
 	goTrimFlags := []string{"-gcflags", "all=-trimpath=${GOPATH}", "-asmflags", "all=-trimpath=${GOPATH}"}
 	absProjectPath := projutil.MustGetwd()
 	projectName := filepath.Base(absProjectPath)
@@ -155,11 +154,9 @@ func buildFunc(cmd *cobra.Command, args []string) error {
 	// Don't need to build Go code if a non-Go Operator.
 	if projutil.IsOperatorGo() {
 		managerDir := filepath.Join(projutil.CheckAndGetProjectGoPkg(), scaffold.ManagerDir)
+		goBuildArgs := append(goTrimFlags, managerDir)
 		outputBinName := filepath.Join(absProjectPath, scaffold.BuildBinDir, projectName)
-		goBuildArgs := append(append([]string{"build"}, goTrimFlags...), "-o", outputBinName, managerDir)
-		buildCmd := exec.Command("go", goBuildArgs...)
-		buildCmd.Env = goBuildEnv
-		if err := projutil.ExecCmd(buildCmd); err != nil {
+		if err := projutil.GoBuild(outputBinName, goBuildArgs...); err != nil {
 			return fmt.Errorf("failed to build operator binary: (%v)", err)
 		}
 	}
@@ -190,9 +187,9 @@ func buildFunc(cmd *cobra.Command, args []string) error {
 	if enableTests {
 		if projutil.GetOperatorType() == projutil.OperatorTypeGo {
 			testBinary := filepath.Join(absProjectPath, scaffold.BuildBinDir, projectName+"-test")
-			goTestBuildArgs := append(append([]string{"test"}, goTrimFlags...), "-c", "-o", testBinary, testLocationBuild+"/...")
+			goTestBuildArgs := append(append([]string{"test"}, goTrimFlags...), "-mod=vendor", "-c", "-o", testBinary, testLocationBuild+"/...")
 			buildTestCmd := exec.Command("go", goTestBuildArgs...)
-			buildTestCmd.Env = goBuildEnv
+			buildTestCmd.Env = append(os.Environ(), "GOOS=linux", "GOARCH=amd64", "CGO_ENABLED=0")
 			if err := projutil.ExecCmd(buildTestCmd); err != nil {
 				return fmt.Errorf("failed to build test binary: (%v)", err)
 			}
--- cmd/operator-sdk/up/local.go
+++ cmd/operator-sdk/up/local.go
@@ -178,17 +178,12 @@ func setupOperatorEnv() error {
 }
 
 func buildLocal(outputBinName string) error {
-	args := []string{"build", "-o", outputBinName}
+	var args []string
 	if ldFlags != "" {
-		args = append(args, "-ldflags", ldFlags)
+		args = []string{"-ldflags", ldFlags}
 	}
 	args = append(args, filepath.Join(scaffold.ManagerDir, scaffold.CmdFile))
-
-	bc := exec.Command("go", args...)
-	if err := projutil.ExecCmd(bc); err != nil {
-		return err
-	}
-	return nil
+	return projutil.GoBuild(outputBinName, args...)
 }
 
 func printVersion() {
