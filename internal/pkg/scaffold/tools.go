0adf30024da2ca41e29416c2280ea6262e630a57
Eric Stroczynski <ericstroczynski@gmail.com>
2019-04-25 10:11:29 -0700

    update tools

--- internal/pkg/scaffold/tools.go
+++ internal/pkg/scaffold/tools.go
@@ -38,10 +38,12 @@ package tools
 
 import (
 	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "k8s.io/code-generator/cmd/conversion-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
+	_ "k8s.io/gengo/args"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+	_ "sigs.k8s.io/controller-tools/pkg/crd/generator"
 )
 `
