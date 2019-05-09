5727fcef77664fc63b86587125909886f9e2088b
Eric Stroczynski <ericstroczynski@gmail.com>
2019-05-09 13:17:24 -0700

    *types.go,internal/pkg/scaffold/crd_test.go: set kubebuilder subresource status annotation in source

--- internal/pkg/scaffold/types.go
+++ internal/pkg/scaffold/types.go
@@ -71,6 +71,7 @@ type {{.Resource.Kind}}Status struct {
 
 // {{.Resource.Kind}} is the Schema for the {{ .Resource.Resource }} API
 // +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
 type {{.Resource.Kind}} struct {
 	metav1.TypeMeta   ` + "`" + `json:",inline"` + "`" + `
 	metav1.ObjectMeta ` + "`" + `json:"metadata,omitempty"` + "`" + `
