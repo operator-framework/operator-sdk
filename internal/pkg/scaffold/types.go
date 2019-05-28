b607bbd634f77e281171774a18e7294481a56317
Mattias Sjöström <mattias.sjostrom@softsense.io>
2019-05-28 16:38:01 +0200

    Update internal/pkg/scaffold/types.go
    Co-Authored-By: Joe Lanford <joe.lanford@gmail.com>

--- internal/pkg/scaffold/types.go
+++ internal/pkg/scaffold/types.go
@@ -56,7 +56,7 @@ import (
 type {{.Resource.Kind}}Spec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
 }
 
 // {{.Resource.Kind}}Status defines the observed state of {{.Resource.Kind}}
