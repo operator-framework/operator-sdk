8a7ad422361738b9990e579a7f221f63533d00d2
Lili Cosic <cosiclili@gmail.com>
2019-06-13 18:45:17 +0200

    test/e2e/memcached_test.go,e2e-helm.sh: Adjust tests

--- test/e2e/memcached_test.go
+++ test/e2e/memcached_test.go
@@ -633,7 +633,7 @@ func memcachedMetricsTest(t *testing.T, f *framework.Framework, ctx *framework.T
 
 	// Make sure metrics Service exists
 	s := v1.Service{}
-	err = f.Client.Get(context.TODO(), types.NamespacedName{Name: operatorName, Namespace: namespace}, &s)
+	err = f.Client.Get(context.TODO(), types.NamespacedName{Name: fmt.Sprintf("%s-metrics", operatorName), Namespace: namespace}, &s)
 	if err != nil {
 		return fmt.Errorf("could not get metrics Service: (%v)", err)
 	}
