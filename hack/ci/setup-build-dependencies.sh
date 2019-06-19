b0b27df51111ab0c3e7ca2ec67c76a23c6993631
Joe Lanford <joe.lanford@gmail.com>
2019-06-19 12:11:15 -0400

    .travis.yml,hack: bumping dep to v0.5.3

--- hack/ci/setup-build-dependencies.sh
+++ hack/ci/setup-build-dependencies.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install dep
-curl -Lo dep https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64 && chmod +x dep && mv dep /usr/local/bin/
+curl -Lo dep https://github.com/golang/dep/releases/download/v0.5.3/dep-linux-amd64 && chmod +x dep && mv dep /usr/local/bin/
 
 # Ensure vendor directory is up-to-date
 make dep
