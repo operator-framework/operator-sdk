a61655f945c0b472b601f80a071e5672288340c9
Joe Lanford <joe.lanford@gmail.com>
2019-06-19 11:42:36 -0400

    hack/ci/setup-build-dependencies.sh: add setup script in SDK repo

---
+++ hack/ci/setup-build-dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Install dep
+curl -Lo dep https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64 && chmod +x dep && mv dep /usr/local/bin/
+
+# Ensure vendor directory is up-to-date
+make dep
