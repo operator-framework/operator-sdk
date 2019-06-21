c500238e47643b9d0ccc9e39bb0a2e56cafb1bb1
Alex Pavel <apavel@redhat.com>
2019-06-21 09:35:54 -0700

    hack/tests,test/e2e: separate testing and scaffolding of E2E tests

---
+++ hack/tests/scaffolding/e2e-go-scaffold.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+source hack/lib/test_lib.sh
+
+ROOTDIR="$(pwd)"
+trap_add 'rm $ROOTDIR/go.mod' EXIT
+GOTMP="$(mktemp -d)"
+trap_add 'rm -rf $GOTMP' EXIT
+BASEPROJECTDIR="/tmp/go-e2e-scaffold"
+IMAGE_NAME="quay.io/example/memcached-operator:v0.0.1"
+
+rm -rf $BASEPROJECTDIR
+mkdir -p $BASEPROJECTDIR
+
+pushd "$BASEPROJECTDIR"
+go run "$ROOTDIR/hack/tests/scaffolding/scaffold-memcached.go" --local-repo $ROOTDIR --image-name=$IMAGE_NAME --local-image
+popd
