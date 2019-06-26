9263967757b5398fac64d16186155f90de21df80
Joe Lanford <joe.lanford@gmail.com>
2019-06-26 17:25:15 -0400

    hack/ci/setup-build-dependencies.sh: add comment and TODO about no-op

--- hack/ci/setup-build-dependencies.sh
+++ hack/ci/setup-build-dependencies.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-#make tidy
+# No-op since the release:golang-1.12 base image currently has all required
+# depedencies to build and test operator-sdk.
+#
+# TODO: pre-fetch modules here with `make tidy` and figure out permissions
+#       issues in unit and sanity tests
+# make tidy
+
+
