3f6e0bcf5956b2f76b177c6ea74cc182e1d0fa61
Alex Pavel <apavel@redhat.com>
2019-07-17 13:49:08 -0700

    ci/tests,test/e2e: log up local errors better

--- test/e2e/_incluster-test-code/memcached_test.go
+++ test/e2e/_incluster-test-code/memcached_test.go
@@ -255,6 +255,12 @@ func MemcachedLocal(t *testing.T) {
 	}
 
 	if err = memcachedScaleTest(t, framework.Global, ctx, 3, 4); err != nil {
+		file, fileErr := ioutil.ReadFile("stderr.txt")
+		if fileErr != nil {
+			t.Logf("Failed to read operator logs after test failure")
+		} else {
+			t.Logf("Operator Logs: %s", string(file))
+		}
 		t.Fatal(err)
 	}
 }
