a025e23a760f5e263d7089880fe6ac87e4581597
Ish Shah <ishah@redhat.com>
2019-08-27 09:24:26 -0700

    conditional syntax v1

--- internal/pkg/scaffold/ansible/dockerfilehybrid.go
+++ internal/pkg/scaffold/ansible/dockerfilehybrid.go
@@ -98,14 +98,10 @@ COPY roles/ ${HOME}/roles/[[ end ]]
 [[- if .Playbook ]]
 COPY playbook.yml ${HOME}/playbook.yml[[ end ]]
 
-[[- if or .Roles .Playbook ]]
+[[- if or (or .Roles .Playbook) .Watches ]]
 # Corrects file permissions to be fully executable and writable
 RUN find ${HOME} -type f -exec chmod -R g+rw {} \; && \
-	find ${HOME} -type d -exec chmod -R g+rwx {} \;
-[[ else if .Watches ]]
-# Corrects file permissions to be fully executable and writable
-RUN find ${HOME} -type f -exec chmod -R g+rw {} \; && \
-    find ${HOME} -type d -exec chmod -R g+rwx {} \;[[ end ]]
+	find ${HOME} -type d -exec chmod -R g+rwx {} \;[[ end ]]
 
 ENTRYPOINT ["/tini", "--", "/usr/local/bin/entrypoint"]
 
