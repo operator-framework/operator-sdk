ac8b9568007e7d8693a4603fd6d3383dba7a72d0
Clare Yang <yangzhares@users.noreply.github.com>
2019-09-09 16:48:36 +0800

    cmd: added new flag --skip-generation for operator-sdk add api (#1890)
    * cmd: added new flag `--skip-generation` for `operator-sdk add api`
    
    To skip to run k8s codegen and openapi-gen to generate deepcopy and validation spec for new CRD
    
    * CHANGELOG.md: added new flag --skip-generation

--- cmd/operator-sdk/add/api.go
+++ cmd/operator-sdk/add/api.go
@@ -31,15 +31,16 @@ import (
 )
 
 var (
-	apiVersion string
-	kind       string
+	apiVersion     string
+	kind           string
+	skipGeneration bool
 )
 
 func newAddApiCmd() *cobra.Command {
 	apiCmd := &cobra.Command{
 		Use:   "api",
 		Short: "Adds a new api definition under pkg/apis",
-		Long: `operator-sdk add api --kind=<kind> --api-version=<group/version> creates the
+		Long: `operator-sdk add api --kind=<kind> --api-version=<group/version> --skip-generation=<true or false> creates the
 api definition for a new custom resource under pkg/apis. This command must be
 run from the project root directory. If the api already exists at
 pkg/apis/<group>/<version> then the command will not overwrite and return an
@@ -50,7 +51,8 @@ types in all paths under pkg/apis. Go code is generated under
 pkg/apis/<group>/<version>/zz_generated.{deepcopy,openapi}.go. CRD's are
 generated, or updated if they exist for a particular group + version + kind,
 under deploy/crds/<group>_<version>_<kind>_crd.yaml; OpenAPI V3 validation YAML
-is generated as a 'validation' object.
+is generated as a 'validation' object. But can disable this by setting flag
+--skip-generation.
 
 Example:
 	$ operator-sdk add api --api-version=app.example.com/v1alpha1 --kind=AppService
@@ -80,6 +82,7 @@ Example:
 	if err := apiCmd.MarkFlagRequired("kind"); err != nil {
 		log.Fatalf("Failed to mark `kind` flag for `add api` subcommand as required")
 	}
+	apiCmd.Flags().BoolVar(&skipGeneration, "skip-generation", false, "Skip to run k8s codegen and openapi-gen to generate deepcopy and validation spec for new CRD (default false)")
 
 	return apiCmd
 }
@@ -132,14 +135,16 @@ func apiRun(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("failed to update the RBAC manifest for the resource (%v, %v): (%v)", r.APIVersion, r.Kind, err)
 	}
 
-	// Run k8s codegen for deepcopy
-	if err := genutil.K8sCodegen(); err != nil {
-		return err
-	}
+	if !skipGeneration {
+		// Run k8s codegen for deepcopy
+		if err := genutil.K8sCodegen(); err != nil {
+			return err
+		}
 
-	// Generate a validation spec for the new CRD.
-	if err := genutil.OpenAPIGen(); err != nil {
-		return err
+		// Generate a validation spec for the new CRD.
+		if err := genutil.OpenAPIGen(); err != nil {
+			return err
+		}
 	}
 
 	log.Info("API generation complete.")
