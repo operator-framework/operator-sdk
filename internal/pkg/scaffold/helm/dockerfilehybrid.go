c3cd1953e02587e3b9486be91d334358a21ffc4e
Camila Macedo <camilamacedo86@gmail.com>
2019-09-10 18:13:50 +0100

    change helm

--- internal/pkg/scaffold/helm/dockerfilehybrid.go
+++ internal/pkg/scaffold/helm/dockerfilehybrid.go
@@ -41,25 +41,58 @@ func (d *DockerfileHybrid) GetInput() (input.Input, error) {
 	return d.Input, nil
 }
 
-const dockerFileHybridHelmTmpl = `FROM registry.access.redhat.com/ubi7/ubi-minimal:latest
+const dockerFileHybridHelmTmpl = `
+#############################################
+# Temp image to build the prerequisites 
+#############################################
+FROM registry.access.redhat.com/ubi8/ubi:latest AS builder
 
-ENV OPERATOR=/usr/local/bin/helm-operator \
+RUN groupadd -g 1001 {{.ProjectName}} && \
+    useradd -m -s /sbin/nologin -u 1001 -g {{.ProjectName}} {{.ProjectName}}
+
+#############################################
+# Container Image
+#############################################
+FROM registry.access.redhat.com/ubi8/ubi-minimal:latest
+MAINTAINER Red Hat Operator Framework <operator-framework@googlegroups.com>
+
+# Define Labels
+LABEL name="operator-framework/operator-sdk-operator-image" \
+      maintainer="operator-framework@googlegroups.com" \
+      vendor="Operator Framework" 
+
+# Define EnvVars
+ENV OPERATOR=/usr/local/bin/{{.ProjectName}} \
     USER_UID=1001 \
-    USER_NAME=helm \
-    HOME=/opt/helm
+    USER_NAME={{.ProjectName}} \
+    USER_HOME=/home/{{.ProjectName}}  
 
-{{- if .Watches }}
-COPY watches.yaml ${HOME}/watches.yaml{{ end }}
+# Copy group and user created in the builder image
+COPY --from=builder /etc/passwd /tmp
+COPY --from=builder /etc/group /tmp
 
-# install operator binary
-COPY build/_output/bin/{{.ProjectName}} ${OPERATOR}
+# Create the user and group in this image and add permissions 
+RUN cat /tmp/passwd | grep ${USER_NAME} >> /etc/passwd \
+    && cat /tmp/group | grep ${USER_NAME} >> /etc/group \
+    && mkdir -p ${USER_HOME} \
+    && chown -R ${USER_NAME}:${USER_NAME} ${USER_HOME} \
+    && chmod ug+rwx ${USER_HOME} \
+    && rm -rf /tmp/passwd \
+    && rm -rf /tmp/group
 
+# Use the user created to run the container as rootless
+USER ${USER_UID}
+
+# Installs the operator binary
+COPY build/_output/bin/${USER_NAME} ${OPERATOR}
 COPY bin /usr/local/bin
-RUN  /usr/local/bin/user_setup
 
 {{- if .HelmCharts }}
 COPY helm-charts/ ${HOME}/helm-charts/{{ end }}
 
+# This allows the Platform to validate the authority the image
+# More info: https://docs.openshift.com/container-platform/3.11/creating_images/guidelines.html#openshift-specific-guidelines
 ENTRYPOINT ["/usr/local/bin/entrypoint"]
 
-USER ${USER_UID}`
+# Execute the operator 
+CMD exec ${OPERATOR} $@`
