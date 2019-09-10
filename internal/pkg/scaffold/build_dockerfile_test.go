ca86ac430a44b86b7169f6a402aec18139be3af6
Camila Macedo <camilamacedo86@gmail.com>
2019-09-10 17:43:30 +0100

    changing dockerfile test image

--- internal/pkg/scaffold/build_dockerfile_test.go
+++ internal/pkg/scaffold/build_dockerfile_test.go
@@ -33,19 +33,55 @@ func TestDockerfile(t *testing.T) {
 	}
 }
 
-const dockerfileExp = `FROM registry.access.redhat.com/ubi7/ubi-minimal:latest
+const dockerfileExp = `
+#############################################
+# Temp image to build the prerequisites 
+#############################################
+FROM registry.access.redhat.com/ubi8/ubi:latest AS builder
 
+RUN groupadd -g 1001 app-operator && \
+    useradd -m -s /sbin/nologin -u 1001 -g app-operator app-operator
+
+#############################################
+# Container Image
+#############################################
+FROM registry.access.redhat.com/ubi8/ubi-minimal:latest
+MAINTAINER Red Hat Operator Framework <operator-framework@googlegroups.com>
+
+# Define Labels
+LABEL name="operator-framework/operator-sdk-operator-image" \
+      maintainer="operator-framework@googlegroups.com" \
+      vendor="Operator Framework" 
+
+# Define EnvVars
 ENV OPERATOR=/usr/local/bin/app-operator \
     USER_UID=1001 \
-    USER_NAME=app-operator
+    USER_NAME=app-operator \
+    USER_HOME=/home/app-operator  
 
-# install operator binary
-COPY build/_output/bin/app-operator ${OPERATOR}
+# Copy group and user created in the builder image
+COPY --from=builder /etc/passwd /tmp
+COPY --from=builder /etc/group /tmp
 
+# Create the user and group in this image and add permissions 
+RUN cat /tmp/passwd | grep ${USER_NAME} >> /etc/passwd \
+    && cat /tmp/group | grep ${USER_NAME} >> /etc/group \
+    && mkdir -p ${USER_HOME} \
+    && chown -R ${USER_NAME}:${USER_NAME} ${USER_HOME} \
+    && chmod ug+rwx ${USER_HOME} \
+    && rm -rf /tmp/passwd \
+    && rm -rf /tmp/group
+
+# Use the user created to run the container as rootless
+USER ${USER_UID}
+
+# Installs the operator binary
+COPY build/_output/bin/${USER_NAME} ${OPERATOR}
 COPY build/bin /usr/local/bin
-RUN  /usr/local/bin/user_setup
 
+# This allows the Platform to validate the authority the image
+# More info: https://docs.openshift.com/container-platform/3.11/creating_images/guidelines.html#openshift-specific-guidelines
 ENTRYPOINT ["/usr/local/bin/entrypoint"]
 
-USER ${USER_UID}
-`
+# Execute the operator 
+CMD exec ${OPERATOR} $@`
