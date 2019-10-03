d4ba7e63cdb1a993895965a8197dd1be9d87d1e3
Camila Macedo <camilamacedo86@gmail.com>
2019-10-03 15:14:46 +0100

    using pip3

--- internal/pkg/scaffold/ansible/dockerfilehybrid.go
+++ internal/pkg/scaffold/ansible/dockerfilehybrid.go
@@ -63,11 +63,10 @@ ENV OPERATOR=/usr/local/bin/ansible-operator \
 # yum clean all && rm -rf /var/yum/cache/* first
 RUN yum clean all && rm -rf /var/cache/yum/* \
  && yum -y update \
- && yum install -y python36-devel gcc python3-pip \
+ && yum install -y python36-devel gcc python3-pip python3-setuptools \
  # todo: remove inotify-tools. More info: See https://github.com/operator-framework/operator-sdk/issues/2007
  && yum install -y https://rpmfind.net/linux/fedora/linux/releases/30/Everything/x86_64/os/Packages/i/inotify-tools-3.14-16.fc30.x86_64.rpm \
- && pip3 install --upgrade setuptools pip \
- && pip install --no-cache-dir --ignore-installed ipaddress \
+ && pip3 install --no-cache-dir --ignore-installed ipaddress \
       ansible-runner==1.3.4 \
       ansible-runner-http==1.0.0 \
       openshift==0.8.9 \
