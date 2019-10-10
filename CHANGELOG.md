872e7d997486bb587660fc8d6226eaab8b5c1087
Ish Shah <ishah@redhat.com>
2019-10-10 13:04:50 -0700

    *: release v0.10.1 (#2038)
    * cherry picked commit
    
    * backport second bug fix
    
    * revert previous backport
    
    * update versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,4 @@
-## Unreleased
-
-### Added
-
-### Changed
-
-### Deprecated
-
-### Removed
+## v0.10.1
 
 ### Bug Fixes
 
--- version/version.go
+++ version/version.go
@@ -15,7 +15,7 @@
 package version
 
 var (
-	Version    = "v0.10.0+git"
+	Version    = "v0.10.1"
 	GitVersion = "unknown"
 	GitCommit  = "unknown"
 )
