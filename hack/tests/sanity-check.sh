d7edd9cc986a67002ffc28831a74e8a752304f5f
Joe Lanford <joe.lanford@gmail.com>
2019-10-16 11:55:28 -0400

    hack/tests/sanity-check.sh: ignore go.mod changes caused by go vet

--- hack/tests/sanity-check.sh
+++ hack/tests/sanity-check.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -ex
 
+# Make sure repo is in clean state before running go vet
+git diff --exit-code
+
 go vet ./...
 go fmt ./...
+
+# Ignore changes to go.mod caused by running go vet
+git checkout go.mod
+
 ./hack/check_license.sh
 ./hack/check_error_log_msg_format.sh
 
-# Make sure repo is in clean state
+# Make sure repo is still in a clean state.
 git diff --exit-code
