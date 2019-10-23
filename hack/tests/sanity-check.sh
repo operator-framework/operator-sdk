92e1bfda3ef4f58ece065434e30ead20b324f2fe
Camila Macedo <camilamacedo86@gmail.com>
2019-10-23 17:33:40 +0100

    fix sanity to run locally by using make targets

--- hack/tests/sanity-check.sh
+++ hack/tests/sanity-check.sh
@@ -4,10 +4,9 @@ set -ex
 # Make sure repo is in clean state before running go tools
 git diff --exit-code
 
-go mod tidy
-go vet ./...
-go fmt ./...
-
+make tidy
+make vet
+make format
 ./hack/check_license.sh
 ./hack/check_error_log_msg_format.sh
 
