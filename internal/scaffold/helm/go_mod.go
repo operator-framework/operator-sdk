17d389050d46b27c6bf24a20fe43f2e04c63105d
Joe Lanford <joe.lanford@gmail.com>
2019-10-24 18:49:24 -0400

    Bump to controller-runtime 0.3.0 and Kubernetes 1.15 (#2083)
    * Bump kubernetes dependencies to 1.15.4
    
    * bump controller-tools to v0.2.2
    
    * CHANGELOG.md: add lines for #2083
    
    * doc/dev/release.md: tweak release instructions for go.mod changes

--- internal/scaffold/helm/go_mod.go
+++ internal/scaffold/helm/go_mod.go
@@ -41,53 +41,32 @@ const goModTmpl = `module {{ .Repo }}
 go 1.13
 
 require (
-	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
-	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
-	github.com/coreos/prometheus-operator v0.31.1 // indirect
-	github.com/docker/docker v0.0.0-20180612054059-a9fbbdc8dd87 // indirect
-	github.com/emicklei/go-restful v2.9.3+incompatible // indirect
-	github.com/go-openapi/spec v0.19.0 // indirect
-	github.com/google/btree v1.0.0 // indirect
-	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/gotestyourself/gotestyourself v2.2.0+incompatible // indirect
-	github.com/gregjones/httpcache v0.0.0-20190212212710-3befbb6ad0cc // indirect
-	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
-	github.com/imdario/mergo v0.3.7 // indirect
-	github.com/jonboulle/clockwork v0.1.0 // indirect
-	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
 	github.com/operator-framework/operator-sdk master
-	github.com/spf13/pflag v1.0.3
-	github.com/ugorji/go/codec v0.0.0-20190320090025-2dc34c0b8780 // indirect
-	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	google.golang.org/appengine v1.5.0 // indirect
-	gotest.tools v2.2.0+incompatible // indirect
-	k8s.io/apiserver v0.0.0-20181213151703-3ccfe8365421 // indirect
-	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
-	k8s.io/kube-openapi v0.0.0-20190603182131-db7b694dc208 // indirect
-	sigs.k8s.io/controller-runtime v0.2.0
+	sigs.k8s.io/controller-runtime v0.3.0
 )
 
-// Pinned to kubernetes-1.14.1
+// Pinned to kubernetes-1.15.4
 replace (
-	k8s.io/api => k8s.io/api kubernetes-1.14.1
-	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver kubernetes-1.14.1
-	k8s.io/apimachinery => k8s.io/apimachinery kubernetes-1.14.1
-	k8s.io/apiserver => k8s.io/apiserver kubernetes-1.14.1
-	k8s.io/cli-runtime => k8s.io/cli-runtime kubernetes-1.14.1
-	k8s.io/client-go => k8s.io/client-go kubernetes-1.14.1
-	k8s.io/cloud-provider => k8s.io/cloud-provider kubernetes-1.14.1
-	k8s.io/kubernetes => k8s.io/kubernetes v1.14.1
-)
-
-replace (
-	// Indirect operator-sdk dependencies use git.apache.org, which is frequently
-	// down. The github mirror should be used instead.
-	// Locking to a specific version (from 'go mod graph'):
-	git.apache.org/thrift.git => github.com/apache/thrift v0.0.0-20180902110319-2566ecd5d999
-	github.com/coreos/prometheus-operator => github.com/coreos/prometheus-operator v0.31.1
-	// Pinned to v2.10.0 (kubernetes-1.14.1) so https://proxy.golang.org can
-	// resolve it correctly.
-	github.com/prometheus/prometheus => github.com/prometheus/prometheus d20e84d0fb64aff2f62a977adc8cfb656da4e286
+	k8s.io/api => k8s.io/api v0.0.0-20190918195907-bd6ac527cfd2
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20190918201827-3de75813f604
+	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
+	k8s.io/apiserver => k8s.io/apiserver v0.0.0-20190918200908-1e17798da8c1
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.0.0-20190918202139-0b14c719ca62
+	k8s.io/client-go => k8s.io/client-go v0.0.0-20190918200256-06eb1244587a
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.0.0-20190918203125-ae665f80358a
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.0.0-20190918202959-c340507a5d48
+	k8s.io/code-generator => k8s.io/code-generator v0.0.0-20190612205613-18da4a14b22b
+	k8s.io/component-base => k8s.io/component-base v0.0.0-20190918200425-ed2f0867c778
+	k8s.io/cri-api => k8s.io/cri-api v0.0.0-20190817025403-3ae76f584e79
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.0.0-20190918203248-97c07dcbb623
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.0.0-20190918201136-c3a845f1fbb2
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.0.0-20190918202837-c54ce30c680e
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.0.0-20190918202429-08c8357f8e2d
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.0.0-20190918202713-c34a54b3ec8e
+	k8s.io/kubelet => k8s.io/kubelet v0.0.0-20190918202550-958285cf3eef
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.0.0-20190918203421-225f0541b3ea
+	k8s.io/metrics => k8s.io/metrics v0.0.0-20190918202012-3c1ca76f5bda
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.0.0-20190918201353-5cc279503896
 )
 `
 
