8ab602c0e149a7c09590bd3d271f7b0c4db0fb58
Camila Macedo <camilamacedo86@gmail.com>
2019-11-01 15:18:40 +0000

    add sanity check to ensure that the test-framework is updated

--- hack/tests/sanity-check.sh
+++ hack/tests/sanity-check.sh
@@ -9,5 +9,21 @@ go fmt ./...
 ./hack/check_error_log_msg_format.sh
 ./hack/doc/gen_cli_doc.sh
 
+# Make sure that test/test-framework is updated according to the current source code
+make install
+cd test/test-framework
+
+# Create files in the mock test/test-framework just for we are able to exec the commands
+cat > build/Dockerfile
+cat > go.mod
+
+GO111MODULE=on operator-sdk generate k8s
+GO111MODULE=on operator-sdk generate openapi
+
+# Remove files that are required and created just for we exec the commands.
+rm -rf build/Dockerfile
+rm -rf go.mod
+rm -rf go.sum
+
 # Make sure repo is still in a clean state.
 git diff --exit-code
