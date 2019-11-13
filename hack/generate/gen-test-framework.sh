97f80b4f4a7aff5d603afc9b90226f0fc78b3668
Eric Stroczynski <ericstroczynski@gmail.com>
2019-11-13 01:44:50 -0800

    test/test-framework: add go.mod, go.sum and build/Dockerfile
    hack/generate/gen-test-framework.sh: remove workarounds for lack
    of go.mod, go.sum, build/Dockerfile
    
    internal/scaffold/crd_test.go: modularizing test/test-framework
    broke the Go project test because submodules are not packages of
    a module, which caused CRD generator errors. Changing wd to
    test/test-framework fixed the issue

--- hack/generate/gen-test-framework.sh
+++ hack/generate/gen-test-framework.sh
@@ -14,13 +14,6 @@ DOCKERFILE="build/Dockerfile"
 # Go inside of the mock data test project
 cd test/test-framework
 
-# Create the required files in the mock test/test-framework just to allow run from it the oprator-sdk commands
-echo > $DOCKERFILE
-go mod init test-framework
-
-# Remove files that are required and created just to exec the commands.
-trap_add 'rm -rf $DOCKERFILE go.mod go.sum' EXIT
-
 # Run gen commands
 ../../build/operator-sdk generate k8s
 ../../build/operator-sdk generate openapi
