3b2f6845f05b89a685fb09f1dcf6ce67db94b64e
Eric Stroczynski <estroczy@redhat.com>
2019-11-19 11:39:08 -0800

    test/test-framework: add go.mod, go.sum and build/Dockerfile (#2202)
    * test/test-framework: add go.mod, go.sum and build/Dockerfile, and
    update CRDs after running generators
    
    * hack/generate/gen-test-framework.sh: remove workarounds for lack
    of go.mod, go.sum, build/Dockerfile
    
    * internal/scaffold/crd_test.go: modularizing test/test-framework
    broke the Go project test because submodules are not packages of
    a module, which caused CRD generator errors. Changing wd to
    test/test-framework fixed the issue. Add Replace for invalid doc link
    
    * release.md: add test/test-framework/go.mod to list of updated files

--- hack/generate/gen-test-framework.sh
+++ hack/generate/gen-test-framework.sh
@@ -8,23 +8,22 @@ set -ex
 
 source hack/lib/test_lib.sh
 
-# Define vars
-DOCKERFILE="build/Dockerfile"
-
 # Go inside of the mock data test project
 cd test/test-framework
 
-# Create the required files in the mock test/test-framework just to allow run from it the oprator-sdk commands
-echo > $DOCKERFILE
-go mod init test-framework
-
-# Remove files that are required and created just to exec the commands.
-trap_add 'rm -rf $DOCKERFILE go.mod go.sum' EXIT
+# Ensure test-framework is up-to-date with current Go project dependencies.
+# NOTE: the SDK dependency version still needs updating on a new release.
+sdk_version="$(go list -m -f '{{.Version}}' github.com/operator-framework/operator-sdk)"
+../../build/operator-sdk print-deps > go.mod
+sed -i 's|github.com/operator-framework/operator-sdk\s*master||g' go.mod
+echo -e "\nreplace github.com/operator-framework/operator-sdk => ../../" >> go.mod
+go mod edit -require "github.com/operator-framework/operator-sdk@${sdk_version}"
+go build ./...
+go mod tidy
 
 # Run gen commands
 ../../build/operator-sdk generate k8s
-../../build/operator-sdk generate openapi
-
 # TODO(camilamacedo86): remove this when the openapi gen be set to false and it no longer is generated
 # The following file is gen by openapi but it has not been committed in order to allow we clone and call the test locally in any path.
 trap_add 'rm pkg/apis/cache/v1alpha1/zz_generated.openapi.go' EXIT
+../../build/operator-sdk generate openapi
