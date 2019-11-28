e31c07f03f53f8bab7423cdb34e895c2d198222a
Dishon Goro <dishon@sendyit.com>
2019-11-28 13:36:19 +0300

    fix failing test

--- internal/scaffold/ansible/dockerfilehybrid.go
+++ internal/scaffold/ansible/dockerfilehybrid.go
@@ -66,8 +66,10 @@ RUN yum clean all && rm -rf /var/cache/yum/* \
  && yum install -y python36-devel gcc python3-pip python3-setuptools \
  # todo: remove inotify-tools. More info: See https://github.com/operator-framework/operator-sdk/issues/2007
  && yum install -y 
+ &&
 https://rpmfind.net/linux/fedora/linux/releases/30/Everything/x86_64/os/Packages/i/inotify-tools-3.14-16.fc30.x86_64.rpm
 \
+
  && pip3 install --no-cache-dir --ignore-installed ipaddress \
       ansible-runner==1.3.4 \
       ansible-runner-http==1.0.0 \
