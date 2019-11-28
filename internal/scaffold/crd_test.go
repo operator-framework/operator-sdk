c9117e5929770b0613bc807ca9390e154fe2f9a4
Dishon Goro <dishon@sendyit.com>
2019-11-28 11:29:00 +0300

    fix failing tests

--- internal/scaffold/crd_test.go
+++ internal/scaffold/crd_test.go
@@ -85,8 +85,7 @@ spec:
         apiVersion:
           description: 'APIVersion defines the versioned schema of this representation
             of an object. Servers should convert recognized schemas to the latest
-            internal value, and may reject unrecognized values. 
-			More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources'
+            internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources'
           type: string
         kind:
           description: 'Kind is a string value representing the REST resource this
