d51d7d33584fb106baa08c8a4cb97118d6d06f74
Joe Lanford <joe.lanford@gmail.com>
2019-12-10 14:46:05 -0500

    *: bump to v0.13.0+git

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ import (
 )
 
 var (
-	Version    = "v0.13.0"
+	Version    = "v0.13.0+git"
 	GitVersion = "unknown"
 	GitCommit  = "unknown"
 	GoVersion  = fmt.Sprintf("%s %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH)
