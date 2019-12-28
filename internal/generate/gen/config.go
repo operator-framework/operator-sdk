83e709998dc0aedf35c8f6c4939567f44ad18a6d
Eric Stroczynski <ericstroczynski@gmail.com>
2019-12-28 11:54:41 -0500

    generalize IncludeFuncs to Filters, can be used for +/- filtering

--- internal/generate/gen/config.go
+++ internal/generate/gen/config.go
@@ -34,12 +34,11 @@ type Config struct {
 	// OutputDir is a dir in which to generate output files. If not set, a
 	// default is used on a per-generator basis.
 	OutputDir string
-	// IncludeFuncs contains a set of filters for paths that a generator
-	// may encounter while gathering data for generation. If any func returns
-	// true, that path will be included by the generator. IncludeFuncs provides
+	// Filters is a set of functional filters for paths that a generator may
+	// encounter while gathering data for generation. Filters provides
 	// fine-grained control over Inputs, since often those paths are often
 	// top-level directories.
-	IncludeFuncs IncludeFuncs
+	Filters FilterFuncs
 }
 
 // GetInputPaths returns all paths in c.Inputs for a set of keys.
@@ -55,14 +54,12 @@ func (c Config) GetInputPaths(keys ...string) (paths []string) {
 	return paths
 }
 
-// IncludeFuncs is a slice of filter funcs. A string passing any func in
-// IncludeFuncs satisfies the filter.
-type IncludeFuncs []func(string) bool
+// FilterFuncs is a slice of filter funcs.
+type FilterFuncs []func(string) bool
 
-// MakeIncludeFuncs creates a set of closures around each path in paths
-// to populate Config.IncludeFuncs. If the argument to the closure has
-// a prefix of path, it returns true.
-func MakeIncludeFuncs(paths ...string) (includes IncludeFuncs) {
+// MakeFilters creates a set of closures around each path in paths.
+// If the argument to a closure has a prefix of path, it returns true.
+func MakeFilters(paths ...string) (filters FilterFuncs) {
 	pathSet := map[string]struct{}{}
 	for _, path := range paths {
 		pathSet[filepath.Clean(path)] = struct{}{}
@@ -72,17 +69,17 @@ func MakeIncludeFuncs(paths ...string) (includes IncludeFuncs) {
 		// Copy the string for the closure.
 		pb := strings.Builder{}
 		pb.WriteString(path)
-		includes = append(includes, func(p string) bool {
+		filters = append(filters, func(p string) bool {
 			// Handle absolute paths referencing the project directory.
 			p = strings.TrimPrefix(p, wd)
 			return strings.HasPrefix(filepath.Clean(p), pb.String())
 		})
 	}
-	return includes
+	return filters
 }
 
-// IsInclude checks if path passes any filter in funcs.
-func (funcs IncludeFuncs) IsInclude(path string) bool {
+// SatisfiesAny returns true if path passes any filter in funcs.
+func (funcs FilterFuncs) SatisfiesAny(path string) bool {
 	for _, f := range funcs {
 		if f(path) {
 			return true
