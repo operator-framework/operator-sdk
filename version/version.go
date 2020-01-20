6058c3d0c45f6a18987f5fa37bd09cb329fc1723
Joe Lanford <joe.lanford@gmail.com>
2020-01-20 12:36:59 -0500

    *: post-v0.14.1 bumps (#2437)

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ import (
 )
 
 var (
-	Version    = "v0.14.1"
+	Version    = "v0.14.1+git"
 	GitVersion = "unknown"
 	GitCommit  = "unknown"
 	GoVersion  = fmt.Sprintf("%s %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH)
