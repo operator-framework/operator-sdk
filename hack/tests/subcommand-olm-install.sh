3a3c53e91865e0fef11b014523aec4dcb98fcc33
Eric Stroczynski <ericstroczynski@gmail.com>
2020-01-21 14:21:45 -0800

    hack/tests: remove 'alpha' subcommand from 'olm' invocations

--- hack/tests/subcommand-olm-install.sh
+++ hack/tests/subcommand-olm-install.sh
@@ -12,27 +12,27 @@ test_version() {
     fi
 
     # Status should fail with OLM not installed
-    commandoutput=$(operator-sdk alpha olm status 2>&1 || true)
+    commandoutput=$(operator-sdk olm status 2>&1 || true)
     echo $commandoutput | grep -F "Failed to get OLM status: no existing installation found"
 
     # Uninstall should fail with OLM not installed
-    commandoutput=$(operator-sdk alpha olm uninstall 2>&1 || true)
+    commandoutput=$(operator-sdk olm uninstall 2>&1 || true)
     echo $commandoutput | grep -F "Failed to uninstall OLM: no existing installation found"
 
     # Install should succeed with nothing installed
-    commandoutput=$(operator-sdk alpha olm install $ver_flag 2>&1)
+    commandoutput=$(operator-sdk olm install $ver_flag 2>&1)
     echo $commandoutput | grep -F "Successfully installed OLM version \\\"${version}\\\""
 
     # Install should fail with OLM Installed
-    commandoutput=$(operator-sdk alpha olm install $ver_flag 2>&1 || true)
+    commandoutput=$(operator-sdk olm install $ver_flag 2>&1 || true)
     echo $commandoutput | grep -F "Failed to install OLM version \\\"${version}\\\": detected existing OLM resources: OLM must be completely uninstalled before installation"
 
     # Status should succeed with OLM installed
-    commandoutput=$(operator-sdk alpha olm status 2>&1)
+    commandoutput=$(operator-sdk olm status 2>&1)
     echo $commandoutput | grep -F "Successfully got OLM status"
 
     # Uninstall should succeed with OLM installed
-    commandoutput=$(operator-sdk alpha olm uninstall 2>&1)
+    commandoutput=$(operator-sdk olm uninstall 2>&1)
     echo $commandoutput | grep -F "Successfully uninstalled OLM"
 }
 
