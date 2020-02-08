c9e16f4237a1d613b24ec92ae7ad898653834872
Camila Macedo <camilamacedo86@gmail.com>
2020-02-08 12:23:43 +0000

    fix ci: for when just docs where updated and arch = ppc64le

--- hack/ci/check-doc-only-update.sh
+++ hack/ci/check-doc-only-update.sh
@@ -5,10 +5,9 @@ set -e
 # Make sure the TRAVIS_COMMIT_RANGE is valid, by catching any errors and exiting.
 if [ -z "$TRAVIS_COMMIT_RANGE" ] || ! git rev-list --quiet $TRAVIS_COMMIT_RANGE; then
   echo "Invalid commit range. Skipping check for doc only update"
-  return 0
-fi
-
-if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md)|(\.MD)|(\.png)|(\.pdf)|^(doc/)|^(MAINTAINERS)|^(LICENSE)'; then
+elif "$SKIP_DOC_CHECK" = "true"; then
+  echo "Skipping check for doc only update. Env Var SKIP_DOC_CHECK = true"
+elif ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md)|(\.MD)|(\.png)|(\.pdf)|^(doc/)|^(MAINTAINERS)|^(LICENSE)'; then
   echo "Only doc files were updated, not running the CI."
   exit 0
 fi
