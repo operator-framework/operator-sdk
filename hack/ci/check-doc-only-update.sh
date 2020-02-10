caf917dcc0ce01faecaccd39328a070382ba98a0
Camila Macedo <camilamacedo86@gmail.com>
2020-02-10 22:58:41 +0000

    fix(ci): do not call shell to skip dploys

--- hack/ci/check-doc-only-update.sh
+++ hack/ci/check-doc-only-update.sh
@@ -5,9 +5,10 @@ set -e
 # Make sure the TRAVIS_COMMIT_RANGE is valid, by catching any errors and exiting.
 if [ -z "$TRAVIS_COMMIT_RANGE" ] || ! git rev-list --quiet $TRAVIS_COMMIT_RANGE; then
   echo "Invalid commit range. Skipping check for doc only update"
-elif "$SKIP_DOC_CHECK" = "true"; then
-  echo "Skipping check for doc only update. Env Var SKIP_DOC_CHECK = true"
-elif ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md)|(\.MD)|(\.png)|(\.pdf)|^(doc/)|^(MAINTAINERS)|^(LICENSE)'; then
+  return 0
+fi
+
+if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md)|(\.MD)|(\.png)|(\.pdf)|^(doc/)|^(MAINTAINERS)|^(LICENSE)'; then
   echo "Only doc files were updated, not running the CI."
   exit 0
 fi
