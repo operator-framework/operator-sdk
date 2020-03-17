0f450e8c007297f50e3f99b72e77ef8f167063f7
Varsha <varshaprasad96@gmail.com>
2020-03-17 16:14:08 -0700

    v0.16.x post release branch setup (#2671)

--- internal/scaffold/go_mod.go
+++ internal/scaffold/go_mod.go
@@ -41,7 +41,7 @@ const goModTmpl = `module {{ .Repo }}
 go 1.13
 
 require (
-	github.com/operator-framework/operator-sdk v0.16.0
+	github.com/operator-framework/operator-sdk v0.16.x
 	sigs.k8s.io/controller-runtime v0.4.0
 )
 
