8eac47a6f9d9c280808174a605c0cd3393d23f8e
Joe Lanford <joe.lanford@gmail.com>
2020-04-06 18:03:29 -0400

    move generate scripts to sub-directories to avoid multiple mains

--- hack/generate/changelog/gen-changelog.sh
+++ hack/generate/changelog/gen-changelog.sh
@@ -5,5 +5,5 @@ set -e
 shopt -s extglob
 
 [[ -n "$GEN_CHANGELOG_TAG" ]] || fatal "Must set GEN_CHANGELOG_TAG (e.g. export GEN_CHANGELOG_TAG=v1.2.3)"
-go run ./hack/generate/gen-changelog.go -tag="${GEN_CHANGELOG_TAG}"
+go run ./hack/generate/changelog/gen-changelog.go -tag="${GEN_CHANGELOG_TAG}"
 rm ./changelog/fragments/!(00-template.yaml)
