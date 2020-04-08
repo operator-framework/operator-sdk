ef12c867e7598bffe8ff8591b421765c20a260ff
bharathi-tenneti <btenneti@redhat.com>
2020-04-08 15:43:36 -0400

    Check for empty gvk

--- pkg/helm/controller/controller.go
+++ pkg/helm/controller/controller.go
@@ -118,6 +118,9 @@ func watchDependentResources(mgr manager.Manager, r *HelmOperatorReconciler, c c
 			}
 
 			gvk := u.GroupVersionKind()
+			if gvk == nil {
+				continue
+			}
 			m.RLock()
 			_, ok := watches[gvk]
 			m.RUnlock()
