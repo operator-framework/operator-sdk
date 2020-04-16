a55d0e4c67b12effaf15d5ca0f8ea6675f277974
Eric Stroczynski <ericstroczynski@gmail.com>
2020-04-15 21:07:55 -0400

    Patch branch post-release v0.17.0 (#2848)

--- version/version.go
+++ version/version.go
@@ -21,7 +21,7 @@ import (
 
 //var needs to be used instead of const for ldflags
 var (
-	Version           = "v0.17.0"
+	Version           = "v0.17.0+git"
 	GitVersion        = "unknown"
 	GitCommit         = "unknown"
 	KubernetesVersion = "unknown"
