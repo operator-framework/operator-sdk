f7baeb916c6a95a069ffa3535ac52eb3df3cb8fa
Eric Stroczynski <ericstroczynski@gmail.com>
2020-04-24 17:54:58 -0700

    hack/ci/check-dock-only-update.sh: ignore all docs

--- hack/ci/check-doc-only-update.sh
+++ hack/ci/check-doc-only-update.sh
@@ -8,7 +8,23 @@ if [ -z "$TRAVIS_COMMIT_RANGE" ] || ! git rev-list --quiet $TRAVIS_COMMIT_RANGE;
   return 0
 fi
 
-if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(\.md)|(\.MD)|(\.png)|(\.pdf)|^(doc/)|^(MAINTAINERS)|^(LICENSE)'; then
+# Patterns to ignore.
+declare -a DOC_PATTERNS
+DOC_PATTERNS=(
+  "(\.md)"
+  "(\.MD)"
+  "(\.png)"
+  "(\.pdf)"
+  "^(doc/)"
+  "^(website/)"
+  "^(changelog/)"
+  "^(OWNERS)"
+  "^(MAINTAINERS)"
+  "^(SECURITY)"
+  "^(LICENSE)"
+)
+
+if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE "$(IFS="|"; echo "${DOC_PATTERNS[*]}")"; then
   echo "Only doc files were updated, not running the CI."
   exit 0
 fi
