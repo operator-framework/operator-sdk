5279f2fcb779f4f39e23e66b1c8d0e027b181f7c
Fabian von Feilitzsch <fabian@fabianism.us>
2020-05-01 11:24:58 -0400

    Add Scorecard2 spec descriptor validation test (#2929)
    * Now validating alm-examples
    
    * Add some todos
    
    * Fix sanity
    
    * Use existing function for pulling CRs from bundle
    
    * Fix imports

--- internal/scorecard/alpha/tests/olm.go
+++ internal/scorecard/alpha/tests/olm.go
@@ -15,6 +15,12 @@
 package tests
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	"github.com/operator-framework/api/pkg/operators"
 	"github.com/operator-framework/operator-registry/pkg/registry"
 	scapiv1alpha2 "github.com/operator-framework/operator-sdk/pkg/apis/scorecard/v1alpha2"
 )
@@ -25,6 +31,8 @@ const (
 	OLMCRDsHaveResourcesTest  = "olm-crds-have-resources"
 	OLMSpecDescriptorsTest    = "olm-spec-descriptors"
 	OLMStatusDescriptorsTest  = "olm-status-descriptors"
+	statusDescriptor          = "status"
+	specDescriptor            = "spec"
 )
 
 // BundleValidationTest validates an on-disk bundle
@@ -70,6 +78,29 @@ func SpecDescriptorsTest(bundle registry.Bundle) scapiv1alpha2.ScorecardTestResu
 	r.State = scapiv1alpha2.PassState
 	r.Errors = make([]string, 0)
 	r.Suggestions = make([]string, 0)
+	csv, err := bundle.ClusterServiceVersion()
+	if err != nil {
+		r.Errors = append(r.Errors, err.Error())
+		r.State = scapiv1alpha2.ErrorState
+		return r
+	}
+	r.Log += fmt.Sprintf("Loaded ClusterServiceVersion: %s\n", csv.GetName())
+	crs, err := GetCRs(bundle)
+	if err != nil {
+		r.Errors = append(r.Errors, err.Error())
+		r.State = scapiv1alpha2.ErrorState
+		return r
+	}
+	r.Log += fmt.Sprintf("Loaded %d Custom Resources from alm-examples\n", len(crs))
+	apiCSV, err := registryToAPICSV(csv)
+	if err != nil {
+		r.Errors = append(r.Errors, err.Error())
+		r.State = scapiv1alpha2.ErrorState
+		return r
+	}
+	for _, cr := range crs {
+		r = checkOwnedCSVDescriptors(cr, apiCSV, specDescriptor, r)
+	}
 	return r
 }
 
@@ -81,5 +112,75 @@ func StatusDescriptorsTest(bundle registry.Bundle) scapiv1alpha2.ScorecardTestRe
 	r.State = scapiv1alpha2.PassState
 	r.Errors = make([]string, 0)
 	r.Suggestions = make([]string, 0)
+	return r
+}
+
+func registryToAPICSV(csv *registry.ClusterServiceVersion) (*operators.ClusterServiceVersion, error) {
+	var apiCSV operators.ClusterServiceVersion
+	csvBytes, err := json.Marshal(csv)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(csvBytes, &apiCSV)
+	if err != nil {
+		return nil, err
+	}
+	return &apiCSV, nil
+}
+
+// TODO This is the validation we did in v1, but it looks like it only validates fields that
+// are in the example CRs, if you have a field in your CRD that isn't present in one of your examples,
+// I don't think it will be validated.
+func checkOwnedCSVDescriptors(cr unstructured.Unstructured, csv *operators.ClusterServiceVersion,
+	descriptor string, r scapiv1alpha2.ScorecardTestResult) scapiv1alpha2.ScorecardTestResult {
+
+	if cr.Object[descriptor] == nil {
+		r.State = scapiv1alpha2.FailState
+		return r
+	}
+
+	block := cr.Object[descriptor].(map[string]interface{})
+
+	var crd *operators.CRDDescription
+	for _, owned := range csv.Spec.CustomResourceDefinitions.Owned {
+		if owned.Kind == cr.GetKind() {
+			crd = &owned
+			break
+		}
+	}
+
+	if crd == nil {
+		msg := fmt.Sprintf("Failed to find an owned CRD for CR %s with GVK %s", cr.GetName(), cr.GroupVersionKind().String())
+		r.Errors = append(r.Errors, msg)
+		r.State = scapiv1alpha2.FailState
+		return r
+	}
+
+	if descriptor == statusDescriptor {
+		for key := range block {
+			for _, statDesc := range crd.StatusDescriptors {
+				if statDesc.Path == key {
+					delete(block, key)
+					break
+				}
+			}
+		}
+	}
+	if descriptor == specDescriptor {
+		for key := range block {
+			for _, specDesc := range crd.SpecDescriptors {
+				if specDesc.Path == key {
+					delete(block, key)
+					break
+				}
+			}
+		}
+	}
+
+	for key := range block {
+		r.Errors = append(r.Errors, fmt.Sprintf("%s does not have a %s descriptor", key, descriptor))
+		r.Suggestions = append(r.Suggestions, fmt.Sprintf("Add a %s descriptor for %s", descriptor, key))
+		r.State = scapiv1alpha2.FailState
+	}
 	return r
 }
