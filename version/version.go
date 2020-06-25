1d14331a8f08a6daee35f21041159727bff27142
Eric Stroczynski <ericstroczynski@gmail.com>
2020-06-25 10:14:01 -0700

    *: post-release v0.18.2

--- version/version.go
+++ version/version.go
@@ -21,7 +21,7 @@ import (
 
 //var needs to be used instead of const for ldflags
 var (
-	Version           = "v0.18.2"
+	Version           = "v0.18.2+git"
 	GitVersion        = "unknown"
 	GitCommit         = "unknown"
 	KubernetesVersion = "unknown"
