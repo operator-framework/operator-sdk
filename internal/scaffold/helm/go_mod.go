53b00d125fb12515cd74fb169149913b401c8995
Eric Stroczynski <estroczy@redhat.com>
2020-06-30 18:45:38 -0400

    *: post release v0.17.2 (#3317)

--- internal/scaffold/helm/go_mod.go
+++ internal/scaffold/helm/go_mod.go
@@ -42,7 +42,7 @@ const goModTmpl = `module {{ .Repo }}
 go 1.13
 
 require (
-	github.com/operator-framework/operator-sdk v0.17.2
+	github.com/operator-framework/operator-sdk v0.17.x
 	sigs.k8s.io/controller-runtime v0.5.2
 )
 
