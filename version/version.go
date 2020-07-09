a84c62bb1b6b23da8f463d9fabeeb4a9aa047cbb
Eric Stroczynski <ericstroczynski@gmail.com>
2020-07-09 13:46:11 -0700

    *: release v0.19.0

--- version/version.go
+++ version/version.go
@@ -21,7 +21,7 @@ import (
 
 //var needs to be used instead of const for ldflags
 var (
-	Version           = "v0.18.0+git"
+	Version           = "v0.19.0"
 	GitVersion        = "unknown"
 	GitCommit         = "unknown"
 	KubernetesVersion = "unknown"
