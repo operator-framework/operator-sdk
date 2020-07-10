412cada355448607926d51b118722aac467e3d09
Joe Lanford <joe.lanford@gmail.com>
2020-07-10 10:10:08 -0400

    hack/tests: remove legacy go e2e test

--- hack/tests/e2e-go.sh
+++ hack/tests/e2e-go.sh
@@ -1,16 +1,43 @@
 #!/usr/bin/env bash
-set -ex
 
-source hack/lib/image_lib.sh
+# remove running containers on exit
+function cleanup() {
+  kind delete cluster
+}
 
-source hack/tests/scaffolding/e2e-go-scaffold.sh
+set -o errexit
+set -o nounset
+set -o pipefail
 
-pushd $BASEPROJECTDIR/memcached-operator
-operator-sdk build $IMAGE_NAME
-# If using a kind cluster, load the image into all nodes.
-load_image_if_kind "$IMAGE_NAME"
+source ./hack/lib/common.sh
+source ./hack/lib/test_lib.sh
 
-operator-sdk test local ./test/e2e
-popd
+test_dir=./test
+tests=$test_dir/e2e-new
 
-go test ./test/e2e/... -root=. -globalMan=testdata/empty.yaml
+export TRACE=1
+export GO111MODULE=on
+
+: ${K8S_VERSION:?"must be set"}
+
+prepare_staging_dir $tmp_sdk_root
+fetch_tools $tmp_sdk_root
+# These envtest environment variables are required for the default unit tests
+# scaffolded in the test operator project. No e2e tests currently use envtest.
+setup_envs $tmp_sdk_root
+build_sdk $tmp_sdk_root
+
+# Create a cluster of version $K8S_VERSION.
+kind create cluster -v 4 --retain --wait=1m \
+  --config $test_dir/kind-config.yaml \
+  --image=kindest/node:$K8S_VERSION
+
+kind export kubeconfig
+
+kubectl cluster-info
+
+docker pull gcr.io/kubebuilder/kube-rbac-proxy:v0.5.0
+kind load docker-image gcr.io/kubebuilder/kube-rbac-proxy:v0.5.0
+
+trap_add cleanup EXIT
+go test -v $tests
