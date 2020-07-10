099df0431105cc429b58ee43facf10e6e2b56a99
Joe Lanford <joe.lanford@gmail.com>
2020-07-10 19:05:54 -0400

    release.sh: remove version checks for non-existent go.mod templates

--- release.sh
+++ release.sh
@@ -38,25 +38,6 @@ if [[ "$VER" != "$CURR_VER" ]]; then
 	exit 1
 fi
 
-GO_GOMOD="internal/scaffold/go_mod.go"
-ANS_GOMOD="internal/scaffold/ansible/go_mod.go"
-HELM_GOMOD="internal/scaffold/helm/go_mod.go"
-CURR_VER_GO_GOMOD="$(sed -E -n -r 's|.*operator-sdk ([^ \t\n]+).*|\1|p' "$GO_GOMOD" | tail -1 | tr -d ' \t\n')"
-if [[ "$VER" != "$CURR_VER_GO_GOMOD" ]]; then
-	echo "go.mod 'replace' entry version is not set correctly in $GO_GOMOD"
-	exit 1
-fi
-CURR_VER_ANS_GOMOD="$(sed -E -n -r 's|.*operator-sdk ([^ \t\n]+).*|\1|p' "$ANS_GOMOD" | tail -1 | tr -d ' \t\n')"
-if [[ "$VER" != "$CURR_VER_ANS_GOMOD" ]]; then
-	echo "go.mod 'replace' entry version is not set correctly in $ANS_GOMOD"
-	exit 1
-fi
-CURR_VER_HELM_GOMOD="$(sed -E -n -r 's|.*operator-sdk ([^ \t\n]+).*|\1|p' "$HELM_GOMOD" | tail -1 | tr -d ' \t\n')"
-if [[ "$VER" != "$CURR_VER_HELM_GOMOD" ]]; then
-	echo "go.mod 'replace' entry version is not set correctly in $HELM_GOMOD"
-	exit 1
-fi
-
 INSTALL_GUIDE_FILE="website/content/en/docs/install-operator-sdk.md"
 CURR_VER_INSTALL_GUIDE_FILE="$(sed -nr 's/.*RELEASE_VERSION=(.+)/\1/p' "$INSTALL_GUIDE_FILE" | tr -d ' \t\n')"
 if [[ "$VER" != "$CURR_VER_INSTALL_GUIDE_FILE" ]]; then
