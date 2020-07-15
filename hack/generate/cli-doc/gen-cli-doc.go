fd3bd8cc4ec35f6e81c0edf938c17940357f1fcb
Venkatramanan Srinivasan <vsriniva@redhat.com>
2020-07-14 19:05:07 -0700

    formatted correctly

--- hack/generate/cli-doc/gen-cli-doc.go
+++ hack/generate/cli-doc/gen-cli-doc.go
@@ -15,17 +15,17 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "path"
-    "path/filepath"
-    "strings"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
-    log "github.com/sirupsen/logrus"
-    "github.com/spf13/cobra"
-    "github.com/spf13/cobra/doc"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 
-    "github.com/operator-framework/operator-sdk/cmd/operator-sdk/cli"
+	"github.com/operator-framework/operator-sdk/cmd/operator-sdk/cli"
 )
 
 const fmTemplate = `---
@@ -54,54 +54,54 @@ func main() {
 // `&lt;` and `&gt;`
 func htmlFormatter(rootCmd *cobra.Command) {
 
-    for _,cmds := range rootCmd.Commands(){
-        if len(cmds.Commands()) > 0 {
-            htmlFormatter(cmds)
-        } 
+	for _, cmds := range rootCmd.Commands() {
+		if len(cmds.Commands()) > 0 {
+			htmlFormatter(cmds)
+		}
 
-        cmds.Long = strings.ReplaceAll(cmds.Long, "<", "&lt;")
-        cmds.Long = strings.ReplaceAll(cmds.Long, ">", "&gt;")
-    }
-    
+		cmds.Long = strings.ReplaceAll(cmds.Long, "<", "&lt;")
+		cmds.Long = strings.ReplaceAll(cmds.Long, ">", "&gt;")
+	}
+
 }
 
 // recreateDocDir removes and recreates the CLI doc directory for rootCmd
 // at docPath to ensure that stale files (e.g. from renamed or removed CLI subcommands)
 // are removed.
 func recreateDocDir(rootCmd *cobra.Command, docPath string) {
-    currentDir, err := os.Getwd()
-    if err != nil {
-        log.Fatalf("Failed to get current directory: %v", err)
-    }
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Failed to get current directory: %v", err)
+	}
 
-    if err := os.Rename(docPath+"/_index.md", currentDir+"/tmp_index.md"); err != nil {
-        log.Fatalf("Failed to move existing index: %v", err)
-    }
-    if err := os.RemoveAll(docPath); err != nil {
-        log.Fatalf("Failed to remove existing generated docs: %v", err)
-    }
-    if err := os.MkdirAll(docPath, 0755); err != nil {
-        log.Fatalf("Failed to re-create docs directory: %v", err)
-    }
+	if err := os.Rename(docPath+"/_index.md", currentDir+"/tmp_index.md"); err != nil {
+		log.Fatalf("Failed to move existing index: %v", err)
+	}
+	if err := os.RemoveAll(docPath); err != nil {
+		log.Fatalf("Failed to remove existing generated docs: %v", err)
+	}
+	if err := os.MkdirAll(docPath, 0755); err != nil {
+		log.Fatalf("Failed to re-create docs directory: %v", err)
+	}
 
-    filePrepender := func(filename string) string {
-        name := filepath.Base(filename)
-        base := strings.TrimSuffix(name, path.Ext(name))
-        return fmt.Sprintf(fmTemplate, strings.Replace(base, "_", " ", -1))
-    }
-    linkHandler := func(name string) string {
-        base := strings.TrimSuffix(name, path.Ext(name))
-        return "../" + base
-    }
-   
-    htmlFormatter(rootCmd)
+	filePrepender := func(filename string) string {
+		name := filepath.Base(filename)
+		base := strings.TrimSuffix(name, path.Ext(name))
+		return fmt.Sprintf(fmTemplate, strings.Replace(base, "_", " ", -1))
+	}
+	linkHandler := func(name string) string {
+		base := strings.TrimSuffix(name, path.Ext(name))
+		return "../" + base
+	}
 
-    err = doc.GenMarkdownTreeCustom(rootCmd, docPath, filePrepender, linkHandler)
-    if err != nil {
-        log.Fatalf("Failed to generate documentation: %v", err)
-    }
+	htmlFormatter(rootCmd)
 
-    if err := os.Rename(currentDir+"/tmp_index.md", docPath+"/_index.md"); err != nil {
-        log.Fatalf("Failed to move existing index: %v", err)
-    }
+	err = doc.GenMarkdownTreeCustom(rootCmd, docPath, filePrepender, linkHandler)
+	if err != nil {
+		log.Fatalf("Failed to generate documentation: %v", err)
+	}
+
+	if err := os.Rename(currentDir+"/tmp_index.md", docPath+"/_index.md"); err != nil {
+		log.Fatalf("Failed to move existing index: %v", err)
+	}
 }
