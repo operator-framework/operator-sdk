f936a6c695bc4598c158762fc1a6cb9e25937da1
Varsha <varshaprasad96@gmail.com>
2020-08-01 00:51:19 -0700

    Post release PR (#3629)
    *: post-release version reset

--- internal/version/version.go
+++ internal/version/version.go
@@ -21,7 +21,7 @@ import (
 
 //var needs to be used instead of const for ldflags
 var (
-	Version           = "v1.0.0-alpha.2"
+	Version           = "v1.0.0-alpha.2+git"
 	GitVersion        = "unknown"
 	GitCommit         = "unknown"
 	KubernetesVersion = "unknown"
