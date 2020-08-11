a1d5990f83d26b1bfb1d79175f3195958896b251
jesus m. rodriguez <jesusr@redhat.com>
2020-08-10 21:21:19 -0400

    post-release version bump

--- internal/version/version.go
+++ internal/version/version.go
@@ -21,7 +21,7 @@ import (
 
 //var needs to be used instead of const for ldflags
 var (
-	Version           = "v1.0.0"
+	Version           = "v1.0.0+git"
 	GitVersion        = "unknown"
 	GitCommit         = "unknown"
 	KubernetesVersion = "unknown"
