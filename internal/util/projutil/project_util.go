3fe021d6838e937429df77191aaf9672f1b115bf
Venkat Ramaraju <venky2063@gmail.com>
2020-08-12 15:00:09 -0700

    Added links

--- internal/util/projutil/project_util.go
+++ internal/util/projutil/project_util.go
@@ -315,7 +315,7 @@ func CheckGoModules() error {
 	}
 	if !goModOn {
 		return fmt.Errorf(`using go modules requires GO111MODULE="on", "auto", or unset.` +
-			` More info: https://sdk.operatorframework.io/docs/golang/quickstart/#a-note-on-dependency-management`)
+			` More info: https://sdk.operatorframework.io/docs/building-operators/golang/quickstart/`)
 	}
 	return nil
 }
