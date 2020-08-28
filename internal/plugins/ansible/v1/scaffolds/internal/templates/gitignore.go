a5c64d3fd339270e4570825b4c473b146da8d008
Camila Macedo <cmacedo@redhat.com>
2020-08-28 19:56:46 +0100

    remove go stuff

--- internal/plugins/ansible/v1/scaffolds/internal/templates/gitignore.go
+++ internal/plugins/ansible/v1/scaffolds/internal/templates/gitignore.go
@@ -47,16 +47,6 @@ const gitignoreTemplate = `
 *.dylib
 bin
 
-# Test binary, build with ` + "`go test -c`" + `
-*.test
-
-# Output of the go coverage tool, specifically when used with LiteIDE
-*.out
-
-# Kubernetes Generated files - skip generated files, except for vendored files
-
-!vendor/**/zz_generated.*
-
 # editor and IDE paraphernalia
 .idea
 *.swp
