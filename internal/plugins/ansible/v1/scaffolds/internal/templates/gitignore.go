9924b6b04fee302c94ebac0fe0cb10af5536c1fd
Camila Macedo <cmacedo@redhat.com>
2020-08-28 20:05:40 +0100

    update the license

--- internal/plugins/ansible/v1/scaffolds/internal/templates/gitignore.go
+++ internal/plugins/ansible/v1/scaffolds/internal/templates/gitignore.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2018 The Kubernetes Authors.
+Modifications copyright 2020 The Operator-SDK Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
