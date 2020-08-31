700b7e3ca527178c683b42d3e1c52a2084eb7a10
Joe Lanford <joe.lanford@gmail.com>
2020-08-31 17:27:29 -0400

    internal/olm/operator/config_test.go: port to ginkgo/gomega

--- internal/olm/operator/config_test.go
+++ internal/olm/operator/config_test.go
@@ -12,96 +12,142 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package operator
+package operator_test
 
 import (
+	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
-	"testing"
 
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/operator-framework/operator-sdk/internal/olm/operator"
 )
 
-// TODO(joelanford): refactor to use ginkgo/gomega
-func TestConfigLoad(t *testing.T) {
-	// create temp kubeconfig files
-	defaultFile, err := ioutil.TempFile("/tmp", "")
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	defer os.Remove(defaultFile.Name())
+const (
+	defaultHost       = "http://localhost:8080"
+	defaultNamespace  = "my-default"
+	customHost        = "https://custom:8443"
+	customNamespace   = "my-custom"
+	overrideNamespace = "my-override"
+)
 
-	if err := ioutil.WriteFile(defaultFile.Name(), []byte(defaultKubeconfig), 0644); err != nil {
-		log.Fatal(err)
-	}
-	clientcmd.RecommendedHomeFile = defaultFile.Name()
+var _ = Describe("Config", func() {
+	Describe("Load", func() {
+		var (
+			c           operator.Configuration
+			defaultFile *os.File
+		)
+		BeforeEach(func() {
+			c = operator.Configuration{}
 
-	customFile, err := ioutil.TempFile("/tmp", "")
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	defer os.Remove(customFile.Name())
-
-	if err := ioutil.WriteFile(customFile.Name(), []byte(testKubeconfig), 0644); err != nil {
-		log.Fatal(err)
-	}
-
-	cases := []struct {
-		name                 string
-		customKubeconfigPath string
-		kubeconfigEnvValue   string
-		namespace            string
-		expectErr            bool
-		expectNamespace      string
-	}{
-		{"default", "", "", "", false, "my-default"},
-		{"custom namespace", "", "", "userspecified", false, "userspecified"},
-		{"non-existent override", "/tmp/doesnotexist", "", "", true, ""},
-		{"custom override", customFile.Name(), "", "", false, "goo"},
-		{"custom KUBECONFIG", "", customFile.Name(), "", false, "goo"},
-	}
-
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			// set KUBECONFIG env var
-			err = os.Setenv("KUBECONFIG", c.kubeconfigEnvValue)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer os.Unsetenv("KUBECONFIG")
-
-			cfg := Configuration{
-				KubeconfigPath: c.customKubeconfigPath,
-				Namespace:      c.namespace,
-			}
-			err := cfg.Load()
-			if err != nil {
-				if !c.expectErr {
-					t.Fatalf("Expected no error, got error: %v", err)
-				}
-			} else {
-				if c.expectErr {
-					t.Fatalf("Expected error, got no error")
-				}
-				if cfg.Namespace != c.expectNamespace {
-					t.Errorf("Wanted namespace %q, got: %q", c.expectNamespace, cfg.Namespace)
-				}
-			}
+			var err error
+			defaultFile, err = ioutil.TempFile("", "operator-sdk-test-kubeconfig")
+			Expect(err).To(BeNil())
+			Expect(ioutil.WriteFile(defaultFile.Name(), genKubeconfig(defaultHost, defaultNamespace), 0644)).To(Succeed())
+			Expect(os.Unsetenv("KUBECONFIG")).To(Succeed())
 		})
-	}
+
+		AfterEach(func() {
+			os.Remove(defaultFile.Name())
+		})
+
+		Context("with no kubeconfig override path or environment variable", func() {
+			It("should fail with non-existent file", func() {
+				clientcmd.RecommendedHomeFile = "/tmp/operator-sdk-kubeconfig-does-not-exist"
+				Expect(c.Load()).To(HaveOccurred())
+			})
+			It("should load the default kubeconfig", func() {
+				clientcmd.RecommendedHomeFile = defaultFile.Name()
+				Expect(c.Load()).To(Succeed())
+				verifyKubeconfig(c, defaultNamespace, defaultHost)
+			})
+			It("should override the namespace", func() {
+				clientcmd.RecommendedHomeFile = defaultFile.Name()
+				c.Namespace = overrideNamespace
+				Expect(c.Load()).To(Succeed())
+				verifyKubeconfig(c, overrideNamespace, defaultHost)
+			})
+		})
+
+		Context("with kubeconfig override", func() {
+			var customFile *os.File
+
+			BeforeEach(func() {
+				var err error
+				customFile, err = ioutil.TempFile("", "operator-sdk-test-kubeconfig")
+				Expect(err).To(BeNil())
+				Expect(ioutil.WriteFile(customFile.Name(), genKubeconfig(customHost, customNamespace), 0644)).To(Succeed())
+				Expect(os.Unsetenv("KUBECONFIG")).To(Succeed())
+				clientcmd.RecommendedHomeFile = defaultFile.Name()
+
+				c = operator.Configuration{}
+			})
+
+			AfterEach(func() {
+				os.Remove(customFile.Name())
+			})
+
+			Context("path", func() {
+				It("should fail with non-existent file", func() {
+					c.KubeconfigPath = "/tmp/operator-sdk-kubeconfig-does-not-exist"
+					Expect(c.Load()).To(HaveOccurred())
+				})
+				It("should load the custom kubeconfig", func() {
+					c.KubeconfigPath = customFile.Name()
+					Expect(c.Load()).To(Succeed())
+					Expect(c.Namespace).To(Equal(customNamespace))
+					verifyKubeconfig(c, customNamespace, customHost)
+				})
+				It("should override the namespace", func() {
+					c.KubeconfigPath = customFile.Name()
+					c.Namespace = overrideNamespace
+					Expect(c.Load()).To(Succeed())
+					verifyKubeconfig(c, overrideNamespace, customHost)
+				})
+			})
+			Context("environment variable", func() {
+				It("should fail with non-existent file", func() {
+					Expect(os.Setenv("KUBECONFIG", "/tmp/operator-sdk-kubeconfig-does-not-exist")).To(Succeed())
+					Expect(c.Load()).To(HaveOccurred())
+				})
+				It("should load the custom kubeconfig", func() {
+					Expect(os.Setenv("KUBECONFIG", customFile.Name())).To(Succeed())
+					Expect(c.Load()).To(Succeed())
+					Expect(c.Namespace).To(Equal(customNamespace))
+					verifyKubeconfig(c, customNamespace, customHost)
+				})
+				It("should override the namespace", func() {
+					Expect(os.Setenv("KUBECONFIG", customFile.Name())).To(Succeed())
+					c.Namespace = overrideNamespace
+					Expect(c.Load()).To(Succeed())
+					verifyKubeconfig(c, overrideNamespace, customHost)
+				})
+			})
+		})
+	})
+})
+
+func verifyKubeconfig(c operator.Configuration, expectedNs, expectedHost string) {
+	Expect(c.Client).NotTo(BeNil())
+	Expect(c.RESTConfig).NotTo(BeNil())
+	Expect(c.RESTConfig.Host).To(Equal(expectedHost))
+	Expect(c.Namespace).To(Equal(expectedNs))
 }
 
-const defaultKubeconfig = `
+func genKubeconfig(host, namespace string) []byte {
+	return []byte(fmt.Sprintf(`
 apiVersion: v1
 clusters:
 - cluster:
-    server: http://localhost:8080
+    server: %s
   name: kubernetes
 contexts:
 - context:
     cluster: kubernetes
-    namespace: my-default
+    namespace: %s
     user: kubernetes-admin
   name: kubernetes-admin@kubernetes
 current-context: kubernetes-admin@kubernetes
@@ -110,33 +156,5 @@ preferences: {}
 users:
 - name: kubernetes-admin
   user:
-`
-
-const testKubeconfig = `
-apiVersion: v1
-clusters:
-- cluster:
-    server: https://192.168.0.130:6443
-  name: kubernetes
-contexts:
-- context:
-    cluster: kubernetes
-    namespace: foo
-    user: kubernetes-admin
-  name: dev
-- context:
-    cluster: kubernetes
-    user: kubernetes-admin
-  name: kubernetes-admin@kubernetes
-- context:
-    cluster: kubernetes
-    namespace: goo
-    user: kubernetes-admin
-  name: prod
-current-context: prod
-kind: Config
-preferences: {}
-users:
-- name: kubernetes-admin
-  user:
-`
+`, host, namespace))
+}
