0e123740a63655cd67ceaa74bd2d392a4cdd5da8
Camila Macedo <camilamacedo86@gmail.com>
2020-09-01 01:07:30 +0100

    cleanup: remove unused code (#3814)

--- internal/util/k8sutil/k8sutil.go
+++ internal/util/k8sutil/k8sutil.go
@@ -29,40 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/yaml"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
-
-// GetKubeconfigAndNamespace returns the *rest.Config and default namespace defined in the
-// kubeconfig at the specified path. If no path is provided, returns the default *rest.Config
-// and namespace
-func GetKubeconfigAndNamespace(configPath string) (*rest.Config, string, error) {
-	var clientConfig clientcmd.ClientConfig
-	var apiConfig *clientcmdapi.Config
-	var err error
-	if configPath != "" {
-		apiConfig, err = clientcmd.LoadFromFile(configPath)
-		if err != nil {
-			return nil, "", fmt.Errorf("failed to load user provided kubeconfig: %v", err)
-		}
-	} else {
-		apiConfig, err = clientcmd.NewDefaultClientConfigLoadingRules().Load()
-		if err != nil {
-			return nil, "", fmt.Errorf("failed to get kubeconfig: %v", err)
-		}
-	}
-	clientConfig = clientcmd.NewDefaultClientConfig(*apiConfig, &clientcmd.ConfigOverrides{})
-	kubeconfig, err := clientConfig.ClientConfig()
-	if err != nil {
-		return nil, "", err
-	}
-	namespace, _, err := clientConfig.Namespace()
-	if err != nil {
-		return nil, "", err
-	}
-	return kubeconfig, namespace, nil
-}
 
 // GetDisplayName turns a project dir name in any of {snake, chain, camel}
 // cases, hierarchical dot structure, or space-delimited into a
