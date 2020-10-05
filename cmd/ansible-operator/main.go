fd771d7eb5109b3d447ce45099b17986e68f31fa
Jared Patrick <jpatrick@zeero.io>
2020-10-05 15:58:49 +0000

    ansible-operator|helm-operator: add the kubernetes auth clients
    This PR adds the kubernetes auth clients to the ansible-operator and
    helm-operator binaries.
    
    Closes #3973

--- cmd/ansible-operator/main.go
+++ cmd/ansible-operator/main.go
@@ -17,6 +17,10 @@ package main
 import (
 	"log"
 
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that `exec-entrypoint` and `run` can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
 	"github.com/spf13/cobra"
 
 	"github.com/operator-framework/operator-sdk/internal/cmd/ansible-operator/run"
