4e42c0a0001b56f070e6ae4f8927e9ee16a4c07b
Austin Macdonald <austin@redhat.com>
2020-11-05 12:17:29 -0500

    Add unit tests for operator_installer.go (#4165)
    Add unit tests for operator_installer.go and olm_resources.go
    
    Signed-off-by: reinvantveer <rein.van.t.veer@geodan.nl>

--- internal/olm/operator/registry/operator_installer_test.go
+++ internal/olm/operator/registry/operator_installer_test.go
@@ -21,7 +21,10 @@ import (
 	. "github.com/onsi/gomega"
 	v1 "github.com/operator-framework/api/pkg/operators/v1"
 	"github.com/operator-framework/api/pkg/operators/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -30,8 +33,167 @@ import (
 )
 
 var _ = Describe("OperatorInstaller", func() {
-	Describe("InstallOperator", func() {
-		// TODO: fill this in once run bundle is done
+	Describe("NewOperatorInstaller", func() {
+		It("should create an OperatorInstaller", func() {
+			cfg := &operator.Configuration{}
+			oi := NewOperatorInstaller(cfg)
+			Expect(oi).ToNot(BeNil())
+		})
+	})
+
+	Describe("createSubscription", func() {
+		var (
+			oi  *OperatorInstaller
+			sch *runtime.Scheme
+		)
+		BeforeEach(func() {
+			// Setup and fake client
+			cfg := &operator.Configuration{}
+			sch = runtime.NewScheme()
+			Expect(v1.AddToScheme(sch)).To(Succeed())
+			Expect(v1alpha1.AddToScheme(sch)).To(Succeed())
+			cfg.Client = fake.NewFakeClientWithScheme(sch)
+
+			oi = NewOperatorInstaller(cfg)
+			oi.StartingCSV = "fakeName"
+			oi.cfg.Namespace = "fakeNS"
+		})
+
+		It("should create the subscription with the fake client", func() {
+			sub, err := oi.createSubscription(context.TODO(), "huzzah")
+			Expect(err).ToNot(HaveOccurred())
+
+			retSub := &v1alpha1.Subscription{}
+			subKey := types.NamespacedName{
+				Namespace: sub.GetNamespace(),
+				Name:      sub.GetName(),
+			}
+			err = oi.cfg.Client.Get(context.TODO(), subKey, retSub)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(retSub.GetName()).To(Equal(sub.GetName()))
+			Expect(retSub.GetNamespace()).To(Equal(sub.GetNamespace()))
+		})
+
+		It("should pass through any client errors (duplicate)", func() {
+
+			sub := newSubscription(oi.StartingCSV, oi.cfg.Namespace, withCatalogSource("duplicate", oi.cfg.Namespace))
+			oi.cfg.Client = fake.NewFakeClientWithScheme(sch, sub)
+
+			_, err := oi.createSubscription(context.TODO(), "duplicate")
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).Should(ContainSubstring("error creating subscription"))
+		})
+	})
+
+	Describe("approveInstallPlan", func() {
+		var (
+			oi  *OperatorInstaller
+			sch *runtime.Scheme
+		)
+		BeforeEach(func() {
+			cfg := &operator.Configuration{}
+			sch = runtime.NewScheme()
+			Expect(v1alpha1.AddToScheme(sch)).To(Succeed())
+			oi = NewOperatorInstaller(cfg)
+		})
+
+		It("should update the install plan", func() {
+			oi.cfg.Client = fake.NewFakeClientWithScheme(sch,
+				&v1alpha1.InstallPlan{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "fakeName",
+						Namespace: "fakeNS",
+					},
+				},
+			)
+
+			ip := &v1alpha1.InstallPlan{}
+			ipKey := types.NamespacedName{
+				Namespace: "fakeNS",
+				Name:      "fakeName",
+			}
+
+			err := oi.cfg.Client.Get(context.TODO(), ipKey, ip)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(ip.Name).To(Equal("fakeName"))
+			Expect(ip.Namespace).To(Equal("fakeNS"))
+
+			// Test
+			sub := &v1alpha1.Subscription{
+				Status: v1alpha1.SubscriptionStatus{
+					InstallPlanRef: &corev1.ObjectReference{
+						Name:      "fakeName",
+						Namespace: "fakeNS",
+					},
+				},
+			}
+			err = oi.approveInstallPlan(context.TODO(), sub)
+			Expect(err).ToNot(HaveOccurred())
+			err = oi.cfg.Client.Get(context.TODO(), ipKey, ip)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(ip.Name).To(Equal("fakeName"))
+			Expect(ip.Namespace).To(Equal("fakeNS"))
+			Expect(ip.Spec.Approved).To(Equal(true))
+		})
+		It("should return an error if the install plan does not exist.", func() {
+			oi.cfg.Client = fake.NewFakeClientWithScheme(sch)
+			sub := &v1alpha1.Subscription{
+				Status: v1alpha1.SubscriptionStatus{
+					InstallPlanRef: &corev1.ObjectReference{
+						Name:      "fakeName",
+						Namespace: "fakeNS",
+					},
+				},
+			}
+			err := oi.approveInstallPlan(context.TODO(), sub)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).Should(ContainSubstring("error getting install plan"))
+		})
+	})
+
+	Describe("waitForInstallPlan", func() {
+		var (
+			oi  *OperatorInstaller
+			sch *runtime.Scheme
+		)
+		BeforeEach(func() {
+			// Setup and fake client
+			cfg := &operator.Configuration{}
+			sch = runtime.NewScheme()
+			Expect(v1alpha1.AddToScheme(sch)).To(Succeed())
+			cfg.Client = fake.NewFakeClientWithScheme(sch)
+
+			oi = NewOperatorInstaller(cfg)
+			oi.StartingCSV = "fakeName"
+			oi.cfg.Namespace = "fakeNS"
+		})
+		It("should return an error if the subscription does not exist.", func() {
+			sub := newSubscription(oi.StartingCSV, oi.cfg.Namespace, withCatalogSource("duplicate", oi.cfg.Namespace))
+
+			err := oi.waitForInstallPlan(context.TODO(), sub)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).Should(ContainSubstring("install plan is not available for the subscription"))
+
+		})
+		It("should return if subscription has an install plan.", func() {
+			sub := &v1alpha1.Subscription{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "fakeName",
+					Namespace: "fakeNS",
+				},
+				Status: v1alpha1.SubscriptionStatus{
+					InstallPlanRef: &corev1.ObjectReference{
+						Name:      "fakeName",
+						Namespace: "fakeNS",
+					},
+				},
+			}
+			err := oi.cfg.Client.Create(context.TODO(), sub)
+			Expect(err).ToNot(HaveOccurred())
+
+			err = oi.waitForInstallPlan(context.TODO(), sub)
+			Expect(err).ToNot(HaveOccurred())
+		})
 	})
 
 	Describe("ensureOperatorGroup", func() {
@@ -212,7 +374,6 @@ var _ = Describe("OperatorInstaller", func() {
 			})
 		})
 	})
-
 	Describe("createOperatorGroup", func() {
 		var (
 			oi     OperatorInstaller
@@ -333,10 +494,6 @@ var _ = Describe("OperatorInstaller", func() {
 			Expect(found).To(BeTrue())
 			Expect(err).Should(BeNil())
 		})
-	})
-
-	Describe("createSubscription", func() {
-		// TODO: add them as part of a different story
 	})
 
 	Describe("getTargetNamespaces", func() {
