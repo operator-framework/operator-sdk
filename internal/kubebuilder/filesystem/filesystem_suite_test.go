f1f5926cc994c73f6f3a3e60aca2785ceaee2557
varshaprasad96 <varshaprasad96@gmail.com>
2020-11-25 10:42:55 -0800

    Rewrite unit tests in internal/kubebuilder/filesystem

--- internal/kubebuilder/filesystem/filesystem_suite_test.go
+++ internal/kubebuilder/filesystem/filesystem_suite_test.go
@@ -12,7 +12,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package machinery
+package filesystem
 
 import (
 	"testing"
@@ -23,5 +23,5 @@ import (
 
 func TestScaffold(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Machinery Suite")
+	RunSpecs(t, "Filesystem Suite")
 }
