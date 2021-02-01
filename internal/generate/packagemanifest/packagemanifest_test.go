82d6baa43711a5beb40311d48af53defa5ef449e
Jonathan Berkhahn <jaberkha@us.ibm.com>
2021-02-01 13:21:42 -0800

    Refactor pkg manifest generator as interface (#3747)
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>
    Signed-off-by: Rein van 't Veer <reinvantveer@gmail.com>

--- internal/generate/packagemanifest/packagemanifest_test.go
+++ internal/generate/packagemanifest/packagemanifest_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The Operator-SDK Authors
+// Copyright 2021 The Operator-SDK Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,263 +12,202 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package packagemanifest
+package packagemanifest_test
 
 import (
-	"bytes"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/format"
-
-	apimanifests "github.com/operator-framework/api/pkg/manifests"
-	"sigs.k8s.io/yaml"
-
-	genutil "github.com/operator-framework/operator-sdk/internal/generate/internal"
+	. "github.com/operator-framework/operator-sdk/internal/generate/packagemanifest"
 )
 
-func TestGenerator(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Generator Suite")
-}
-
-const (
-	pkgManDefaultContent = `channels:
-  - currentCSV: memcached-operator.v0.0.1
-    name: alpha
+var _ = Describe("A package manifest generator", func() {
+	var (
+		testDataDir string
+	)
+	BeforeEach(func() {
+		testDataDir = filepath.Join("..", "testdata")
+	})
+	Describe("Generate", func() {
+		var (
+			g                                    Generator
+			blankOpts                            Options
+			operatorName                         string
+			outputDir                            string
+			pkgManFilename                       string
+			pkgManDefault                        string
+			pkgManOneChannel                     string
+			pkgManUpdatedOneChannel              string
+			pkgManUpdatedSecondChannel           string
+			pkgManUpdatedSecondChannelNewDefault string
+		)
+		BeforeEach(func() {
+			g = NewGenerator()
+			operatorName = "memcached-operator"
+			blankOpts = Options{}
+			pkgManFilename = operatorName + ".package.yaml"
+			outputDir = os.TempDir()
+			pkgManDefault = `channels:
+- currentCSV: memcached-operator.v0.0.1
+  name: alpha
 defaultChannel: alpha
 packageName: memcached-operator
 `
-
-	pkgManSingleChannelContent = `channels:
-  - currentCSV: memcached-operator.v0.0.1
-    name: stable
+			pkgManOneChannel = `channels:
+- currentCSV: memcached-operator.v0.0.1
+  name: stable
 defaultChannel: stable
 packageName: memcached-operator
 `
-)
-
-var (
-	testDataDir = filepath.Join("..", "testdata")
-
-	pkgManDefault, pkgManSingleChannel *apimanifests.PackageManifest
-)
-
-var _ = BeforeSuite(func() {
-	initTestPackageManifestsHelper()
-})
-
-var _ = Describe("Generating a PackageManifest", func() {
-	format.UseStringerRepresentation = true
-
-	var (
-		g            Generator
-		buf          *bytes.Buffer
-		operatorName = "memcached-operator"
-		version      = "0.0.1"
-	)
-
-	BeforeEach(func() {
-		buf = &bytes.Buffer{}
-	})
-
-	Describe("for the new Go project layout", func() {
-
-		Context("with correct Options", func() {
-
-			var (
-				tmp string
-				err error
-			)
-
-			BeforeEach(func() {
-				tmp, err = ioutil.TempDir(".", "")
-				Expect(err).ToNot(HaveOccurred())
+			pkgManUpdatedOneChannel = `channels:
+- currentCSV: memcached-operator.v0.0.2
+  name: alpha
+defaultChannel: alpha
+packageName: memcached-operator
+`
+			pkgManUpdatedSecondChannel = `channels:
+- currentCSV: memcached-operator.v0.0.1
+  name: alpha
+- currentCSV: memcached-operator.v0.0.2
+  name: stable
+defaultChannel: alpha
+packageName: memcached-operator
+`
+			pkgManUpdatedSecondChannelNewDefault = `channels:
+- currentCSV: memcached-operator.v0.0.1
+  name: alpha
+- currentCSV: memcached-operator.v0.0.2
+  name: stable
+defaultChannel: stable
+packageName: memcached-operator
+`
+		})
+		Context("when writing a new package manifest", func() {
+			It("writes a package manifest", func() {
+				err := g.Generate(operatorName, "0.0.1", outputDir, blankOpts)
+				Expect(err).NotTo(HaveOccurred())
+				file, err := ioutil.ReadFile(outputDir + string(os.PathSeparator) + pkgManFilename)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(file)).To(Equal(pkgManDefault))
 			})
+			It("writes a package manifest with a non-default channel", func() {
+				opts := Options{
+					ChannelName: "stable",
+				}
 
-			AfterEach(func() {
-				if tmp != "" {
-					os.RemoveAll(tmp)
-				}
-			})
-
-			It("should write a PackageManifest to an io.Writer", func() {
-				g = Generator{
-					OperatorName: operatorName,
-					Version:      version,
-				}
-				opts := []Option{
-					WithBase(testDataDir),
-					WithWriter(buf),
-				}
-				Expect(g.Generate(opts...)).ToNot(HaveOccurred())
-				Expect(buf.String()).To(MatchYAML(pkgManDefaultContent))
-			})
-			It("should write a PackageManifest to disk", func() {
-				g = Generator{
-					OperatorName: operatorName,
-					Version:      version,
-				}
-				opts := []Option{
-					WithBase(testDataDir),
-					WithFileWriter(tmp),
-				}
-				Expect(g.Generate(opts...)).ToNot(HaveOccurred())
-				outputFile := filepath.Join(tmp, makePkgManFileName(operatorName))
-				Expect(outputFile).To(BeAnExistingFile())
-				Expect(string(readFileHelper(outputFile))).To(MatchYAML(pkgManDefaultContent))
+				err := g.Generate(operatorName, "0.0.1", outputDir, opts)
+				Expect(err).NotTo(HaveOccurred())
+				file, err := ioutil.ReadFile(outputDir + string(os.PathSeparator) + pkgManFilename)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(file)).To(Equal(pkgManOneChannel))
 			})
 		})
+		Context("when updating an existing package manifest", func() {
+			It("creates a new package manifest if provided an existing packagemanifest that doesn't exist", func() {
+				opts := Options{
+					BaseDir:     "testpotato",
+					ChannelName: "stable",
+				}
 
-		Context("with incorrect configuration", func() {
+				err := g.Generate(operatorName, "0.0.1", outputDir, opts)
+				Expect(err).NotTo(HaveOccurred())
+				file, err := ioutil.ReadFile(outputDir + string(os.PathSeparator) + pkgManFilename)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(file)).To(Equal(pkgManOneChannel))
+			})
+			It("updates an existing package manifest with a updated channel", func() {
+				opts := Options{
+					BaseDir:     testDataDir,
+					ChannelName: "alpha",
+				}
 
-			BeforeEach(func() {
-				g = Generator{
-					OperatorName: operatorName,
-					Version:      version,
-				}
+				err := g.Generate(operatorName, "0.0.2", outputDir, opts)
+				Expect(err).NotTo(HaveOccurred())
+				file, err := ioutil.ReadFile(outputDir + string(os.PathSeparator) + pkgManFilename)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(file)).To(Equal(pkgManUpdatedOneChannel))
 			})
+			It("updates an existing package manifest with a new channel", func() {
+				opts := Options{
+					BaseDir:     testDataDir,
+					ChannelName: "stable",
+				}
 
-			It("should return an error without any Options", func() {
-				opts := []Option{}
-				Expect(g.Generate(opts...)).To(MatchError(errNoGetWriter))
+				err := g.Generate(operatorName, "0.0.2", outputDir, opts)
+				Expect(err).NotTo(HaveOccurred())
+				file, err := ioutil.ReadFile(outputDir + string(os.PathSeparator) + pkgManFilename)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(file)).To(Equal(pkgManUpdatedSecondChannel))
 			})
-			It("should return an error without a getWriter", func() {
-				opts := []Option{
-					WithBase(testDataDir),
-				}
-				Expect(g.Generate(opts...)).To(MatchError(errNoGetWriter))
-			})
-			It("should return an error without a getBase", func() {
-				opts := []Option{
-					WithWriter(&bytes.Buffer{}),
-				}
-				Expect(g.Generate(opts...)).To(MatchError(errNoGetBase))
-			})
-			It("should return an error without a Version", func() {
-				g.Version = ""
-				opts := []Option{
-					WithBase(testDataDir),
-					WithWriter(&bytes.Buffer{}),
-				}
-				Expect(g.Generate(opts...)).To(MatchError(errNoVersion))
-			})
-		})
-
-		Context("to create a new PackageManifest", func() {
-			It("should return the default file", func() {
-				g = Generator{
-					OperatorName: operatorName,
-					Version:      version,
-					getBase:      makeBaseGetter(pkgManDefaultContent),
-				}
-				pkg, err := g.generate()
-				Expect(err).ToNot(HaveOccurred())
-				Expect(pkg).To(Equal(pkgManDefault))
-			})
-			It("should return a PackageManifest with a non-default channel", func() {
-				g = Generator{
-					OperatorName: operatorName,
-					Version:      version,
-					ChannelName:  "stable",
-					getBase:      makeBaseGetter(pkgManSingleChannelContent),
-				}
-				pkg, err := g.generate()
-				Expect(err).ToNot(HaveOccurred())
-				Expect(pkg).To(Equal(pkgManSingleChannel))
-			})
-		})
-
-		Context("to update an existing PackageManifest file", func() {
-			It("should return a PackageManifest with one updated channel CSV name", func() {
-				g = Generator{
-					OperatorName: operatorName,
-					Version:      "0.0.2",
-					ChannelName:  "alpha",
-					getBase:      makeBaseGetter(pkgManDefaultContent),
-				}
-				pkg, err := g.generate()
-				Expect(err).ToNot(HaveOccurred())
-				Expect(pkg).To(Equal(&apimanifests.PackageManifest{
-					Channels: []apimanifests.PackageChannel{
-						{Name: "alpha", CurrentCSVName: genutil.MakeCSVName(operatorName, "0.0.2")},
-					},
-					DefaultChannelName: "alpha",
-					PackageName:        operatorName,
-				}))
-			})
-			It("should return a PackageManifest with two channels", func() {
-				g = Generator{
-					OperatorName: operatorName,
-					Version:      "0.0.2",
-					ChannelName:  "stable",
-					getBase:      makeBaseGetter(pkgManDefaultContent),
-				}
-				pkg, err := g.generate()
-				Expect(err).ToNot(HaveOccurred())
-				Expect(pkg).To(Equal(&apimanifests.PackageManifest{
-					Channels: []apimanifests.PackageChannel{
-						{Name: "alpha", CurrentCSVName: genutil.MakeCSVName(operatorName, version)},
-						{Name: "stable", CurrentCSVName: genutil.MakeCSVName(operatorName, "0.0.2")},
-					},
-					DefaultChannelName: "alpha",
-					PackageName:        operatorName,
-				}))
-			})
-			It("should return a PackageManifest with two channels and an updated default channel", func() {
-				g = Generator{
-					OperatorName:     operatorName,
-					Version:          "0.0.2",
+			It("updates an existing package manifest with a new channel and an updated default channel", func() {
+				opts := Options{
+					BaseDir:          testDataDir,
 					ChannelName:      "stable",
 					IsDefaultChannel: true,
-					getBase:          makeBaseGetter(pkgManDefaultContent),
 				}
-				pkg, err := g.generate()
-				Expect(err).ToNot(HaveOccurred())
-				Expect(pkg).To(Equal(&apimanifests.PackageManifest{
-					Channels: []apimanifests.PackageChannel{
-						{Name: "alpha", CurrentCSVName: genutil.MakeCSVName(operatorName, version)},
-						{Name: "stable", CurrentCSVName: genutil.MakeCSVName(operatorName, "0.0.2")},
-					},
-					DefaultChannelName: "stable",
-					PackageName:        operatorName,
-				}))
+
+				err := g.Generate(operatorName, "0.0.2", outputDir, opts)
+				Expect(err).NotTo(HaveOccurred())
+				file, err := ioutil.ReadFile(outputDir + string(os.PathSeparator) + pkgManFilename)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(file)).To(Equal(pkgManUpdatedSecondChannelNewDefault))
 			})
 		})
-
+		Context("when incorrect params are provided", func() {
+			It("fails if no operator name is specified", func() {
+				err := g.Generate("", "", "", blankOpts)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring(ErrNoOpName.Error()))
+			})
+			It("fails if no version is specified", func() {
+				err := g.Generate(operatorName, "", "", blankOpts)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring(ErrNoVersion.Error()))
+			})
+			It("fails if no output directory is set", func() {
+				err := g.Generate(operatorName, "0.0.1", "", blankOpts)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring(ErrNoOutputDir.Error()))
+			})
+		})
 	})
+	Describe("GetBase", func() {
+		var (
+			b PackageManifest
+		)
+		BeforeEach(func() {
+			b = PackageManifest{}
+		})
+		It("returns a new blank packagemanifest", func() {
+			b.PackageName = "sweetsop"
 
+			pm, err := b.GetBase()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(pm).NotTo(BeNil())
+			Expect(pm.PackageName).To(Equal(b.PackageName))
+		})
+		It("reads an existing packagemanifest from disk", func() {
+			b.BasePath = filepath.Join(testDataDir, "memcached-operator.package.yaml")
+
+			pm, err := b.GetBase()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(pm).NotTo(BeNil())
+			Expect(pm.PackageName).To(Equal("memcached-operator"))
+			Expect(len(pm.Channels)).To(Equal(1))
+			Expect(pm.Channels[0].Name).To(Equal("alpha"))
+			Expect(pm.Channels[0].CurrentCSVName).To(Equal("memcached-operator.v0.0.1"))
+			Expect(pm.DefaultChannelName).To(Equal("alpha"))
+		})
+		It("fails if provided a non-existent base path", func() {
+			b.BasePath = "not-a-real-thing.yaml"
+
+			pm, err := b.GetBase()
+			Expect(pm).To(BeNil())
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("error reading existing"))
+		})
+	})
 })
-
-func makeBaseGetter(content string) getBaseFunc {
-	return func() (*apimanifests.PackageManifest, error) {
-		return marshalContent(content)
-	}
-}
-
-func initTestPackageManifestsHelper() {
-	var err error
-	pkgManDefault, err = marshalContent(pkgManDefaultContent)
-	ExpectWithOffset(1, err).ToNot(HaveOccurred())
-	pkgManSingleChannel, err = marshalContent(pkgManSingleChannelContent)
-	ExpectWithOffset(1, err).ToNot(HaveOccurred())
-}
-
-func readFileHelper(path string) []byte {
-	b, err := ioutil.ReadFile(path)
-	ExpectWithOffset(1, err).ToNot(HaveOccurred())
-	return b
-}
-
-func marshalContent(content string) (*apimanifests.PackageManifest, error) {
-	base := &apimanifests.PackageManifest{}
-	if content == "" {
-		return base, nil
-	}
-	err := yaml.Unmarshal([]byte(content), base)
-	return base, err
-}
