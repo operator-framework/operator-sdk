9abbec8f6ca2ecddf0026e0dca80044ea740d34f
jesus m. rodriguez <jmrodri@gmail.com>
2021-02-03 18:39:32 -0500

    React to new expectation.
    Signed-off-by: jesus m. rodriguez <jmrodri@gmail.com>

--- internal/olm/operator/registry/configmap/deployment_test.go
+++ internal/olm/operator/registry/configmap/deployment_test.go
@@ -28,7 +28,7 @@ var _ = Describe("Deployment", func() {
 
 	Describe("getRegistryServerName", func() {
 		It("should return the formatted servername", func() {
-			Expect(getRegistryServerName("pkgName")).Should(Equal("pkgName-registry-server"))
+			Expect(getRegistryServerName("pkgName")).Should(Equal("pkgname-registry-server"))
 			// This checks if all the special characters are handled correctly
 			Expect(getRegistryServerName("$abc.foo$@(&#(&!*)@&#")).Should(Equal("abc-foo-registry-server"))
 		})
