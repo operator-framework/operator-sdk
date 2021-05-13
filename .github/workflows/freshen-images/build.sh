680009e24151d197acacc036811f31372c47f459
Eric Stroczynski <ericstroczynski@gmail.com>
2021-05-13 12:06:18 -0700

    add checkout step and split script into lib and runner
    Signed-off-by: Eric Stroczynski <ericstroczynski@gmail.com>

--- .github/workflows/freshen-images/build.sh
+++ .github/workflows/freshen-images/build.sh
@@ -1,88 +1,8 @@
 #!/usr/bin/env bash
 
-# _buildx runs "docker buildx build" in CI-output mode.
-function _buildx() {
-  echo -e "\n$ docker buildx build --progress plain $@"
-  docker buildx build --progress plain $@
-}
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-# _pull runs "docker pull".
-function _pull() {
-  echo -e "\n$ docker pull $@"
-  docker pull $@
-}
-
-# cmp_times returns false if $2 occurred within some timespan relative to $1.
-# The TIMESPAN variable
-function cmp_times() {
-  local base_seconds=$(date -d "$1" +%s)
-  local img_time_seconds=$(date -d "$2" +%s)
-  if (( $base_seconds - $TIMESPAN < $img_time_seconds )) || (( $FORCE )); then
-    return 1
-  fi
-  return 0
-}
-
-# is_dockerfile_fresh returns false if at least one image in a "FROM" directive
-# in the Dockerfile at $1 has been freshly built within TIMESPAN relative to now,
-# or FORCE=1.
-function is_dockerfile_fresh() {
-  local dockerfile=$1
-  # Strip flag from FROM to get image, which always precedes this flag if set.
-  local docker_images=$(grep -oP "FROM (--platform=[^ ]+ )?\K([^ ]+)" $dockerfile)
-
-  for img in $docker_images; do
-    _pull $img
-    local img_create_time=$(docker inspect --format '{{.Created}}' $img)
-    if [[ "$img_create_time" == "0001-01-01T00:00:00Z" ]]; then 
-      echo "image creation time could be found for $img"
-      exit 1
-    fi
-    if ! cmp_times "$(date)" "$img_create_time"; then
-      return 1
-    fi
-  done
-}
-
-# Build an image at path ./images/ansible-operator/base.Dockerfile checked out at git tag $1
-# for all platforms in $2. Semantics are otherwise the same as build_generic.
-function build_ansible_base() {
-  local tag=$1
-  local platforms=$2
-  local dockerfile=./images/ansible-operator/base.Dockerfile
-
-  git checkout refs/tags/$tag
-  local ansible_base_image_tag=$(grep -oP 'FROM \K(quay\.io/estroz/ansible-operator-base:.+)' ./images/ansible-operator/Dockerfile)
-  # Attempt to get the git ref that built this image from the git_commit image label,
-  # falling back to parsing it from the image tag, which typically contains a git ref
-  # as the last hyphen-delimit element.
-  local ansible_base_git_ref=$(docker inspect --format '{{ index .Config.Labels "git_commit" }}' $ansible_base_image_tag)
-  if [[ $ansible_base_git_ref == "devel" || $ansible_base_git_ref == "" ]]; then
-    ansible_base_git_ref=$(echo $ansible_base_image_tag | sed -E 's|.+:.+-(.+)|\1|')
-  fi
-  git checkout $ansible_base_git_ref
-  if ! is_dockerfile_fresh "$dockerfile"; then
-    _buildx --tag $ansible_base_image_tag --platform "$platforms" --file "$dockerfile" $IMAGE_DO --build-arg GIT_COMMIT=$ansible_base_git_ref ./images/ansible-operator
-  fi
-}
-
-# Build an image at path ./images/$2/Dockerfile checked out at git tag $1
-# for all platforms in $3. Tag is assumed to be "v"+semver; the image is tagged
-# with the full semver string and with "v${major}.${minor}".
-# The build will only run if the Dockerfile is not fresh.
-function build_generic() {
-  local tag=$1
-  local id=$2
-  local platforms=$3
-  local tag_maj_min="quay.io/estroz/${id}:$(echo $tag | grep -Eo "v[1-9]+\.[0-9]+")"
-  local tag_full="quay.io/estroz/${id}:${tag}"
-  local dockerfile=./images/${id}/Dockerfile
-
-  git checkout refs/tags/$tag
-  if ! is_dockerfile_fresh "$dockerfile"; then
-    _buildx --tag "$tag_maj_min" --tag "$tag_full"  --platform "$platforms" --file "$dockerfile" $IMAGE_DO .
-  fi
-}
+source ${DIR}/lib.sh
 
 set -eu
 set -o pipefail
@@ -94,7 +14,9 @@ DEFAULT_PLATFORMS="linux/amd64,linux/arm64,linux/ppc64le,linux/s390x"
 # DEFAULT_SCORECARD_KUTTL_PLATFORMS="linux/amd64,linux/arm64,linux/ppc64le"
 PLATFORMS=
 # Time window to compare image creation times against, relative to now.
+# --timespan should be set to this duration in seconds.
 DEFAULT_TIMESPAN=86400 # 24 hours in seconds
+TIMESPAN=
 # What to do with the image, either load (default) or push.
 IMAGE_DO=--load
 # Space-separated list of git tags.
@@ -124,6 +46,10 @@ while [[ $# -gt 0 ]]; do
   PLATFORMS=$2
   shift
   ;;
+  --timespan)
+  TIMESPAN=$2
+  shift
+  ;;
   *) echo "Invalid flag $1"; exit 1 ;;
   esac
   shift
@@ -151,8 +77,6 @@ tmp=$(mktemp -d --tmpdir freshen-images-tmp.XXXXXX)
 git clone https://github.com/operator-framework/operator-sdk.git $tmp
 trap "rm -rf $tmp" EXIT
 pushd $tmp
-
-set -x
 
 case $IMAGE_ID in
 ansible-operator)
