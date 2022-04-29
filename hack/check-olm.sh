5c0ff0349bea24169f64eee9ad49dd922357c37a
Bryce Palmer <bpalmer@redhat.com>
2022-04-29 11:08:55 -0400

    cleanup olm-test dir sooner
    Signed-off-by: Bryce Palmer <bpalmer@redhat.com>

--- hack/check-olm.sh
+++ hack/check-olm.sh
@@ -23,9 +23,13 @@ done
 
 VERSIONS_ARRAY=($(echo "$VERSIONS_STRING" | tr ' ' '\n'))
 
+# clean up OLM because we are done with it now
+cd ../../
+rm -rf olm-test
+
 # check Makefile OLM_VERSIONS
 EXPECTED="OLM_VERSIONS =$VERSIONS_STRING"
-ACTUAL=$(cat ../../Makefile | grep "OLM_VERSIONS =")
+ACTUAL=$(cat Makefile | grep "OLM_VERSIONS =")
 
 if [[ $ACTUAL != $EXPECTED ]]
 then
@@ -34,7 +38,7 @@ then
 fi
 
 # check internal/bindata/olm/versions.go
-AVAILABLE_VERSIONS=($(cat ../../internal/bindata/olm/versions.go | awk '/var availableVersions/,/},\n}/' | tr -d '\t' | tr -d ' ":{},'))
+AVAILABLE_VERSIONS=($(cat internal/bindata/olm/versions.go | awk '/var availableVersions/,/},\n}/' | tr -d '\t' | tr -d ' ":{},'))
 
 ACTUAL=""
 for i in {1..3}
@@ -50,7 +54,7 @@ fi
 
 # check internal/testutils/olm.go
 EXPECTED="OlmVersionForTestSuite = \"${VERSIONS_ARRAY[2]}\""
-ACTUAL=$(cat ../../internal/testutils/olm.go | grep "OlmVersionForTestSuite =" | tr -d '\t')
+ACTUAL=$(cat internal/testutils/olm.go | grep "OlmVersionForTestSuite =" | tr -d '\t')
 
 if [[ $ACTUAL != $EXPECTED ]]
 then
@@ -60,18 +64,13 @@ fi
 
 # check docs - website/content/en/docs/overview/_index.md
 EXPECTED="Currently, the officially supported OLM Versions are: ${VERSIONS_ARRAY[0]}, ${VERSIONS_ARRAY[1]}, and ${VERSIONS_ARRAY[2]}"
-ACTUAL=$(cat ../../website/content/en/docs/overview/_index.md | grep "Currently, the officially supported OLM Versions are:")
+ACTUAL=$(cat website/content/en/docs/overview/_index.md | grep "Currently, the officially supported OLM Versions are:")
 
 if [[ $ACTUAL != $EXPECTED ]]
 then
     echo -e "\nDocs (website/content/en/docs/overview/_index.md) does not have the most up to date OLM release versions.\nEXPECTED: $EXPECTED | ACTUAL: $ACTUAL"
     FAILED="true"
 fi
-
-
-# clean up 
-cd ../../
-rm -rf olm-test
 
 # State pass or fail result
 if [[ $FAILED != "false" ]]
