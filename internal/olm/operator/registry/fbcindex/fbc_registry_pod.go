1ef0b2065e3e09c43f561054c96e17d097c4894a
rashmigottipati <chowdary.grashmi@gmail.com>
2022-05-24 15:01:18 -0400

    Create test ConfigMap and add VolumeMount to container's PodSpec
    Signed-off-by: rashmigottipati <chowdary.grashmi@gmail.com>

--- internal/olm/operator/registry/fbcindex/fbc_registry_pod.go
+++ internal/olm/operator/registry/fbcindex/fbc_registry_pod.go
@@ -44,6 +44,10 @@ const (
 
 	defaultContainerName     = "registry-grpc"
 	defaultContainerPortName = "grpc"
+
+	// The FBC directory that exists under root of an FBC container image.
+	// This directory has the File-Based Catalog representation of a catalog index.
+	defaultFBCIndexRootDir = "/configs"
 )
 
 // FBCRegistryPod holds resources necessary for creation of a registry pod in FBC scenarios.
@@ -194,6 +198,21 @@ func (f *FBCRegistryPod) podForBundleRegistry() (*corev1.Pod, error) {
 		return nil, err
 	}
 
+	// create a ConfigMap
+	cm := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: corev1.SchemeGroupVersion.String(),
+			Kind:       "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "operator-sdk-run-bundle-config",
+			Namespace: f.cfg.Namespace,
+		},
+		Data: map[string]string{
+			"hello": "world",
+		},
+	}
+
 	// make the pod definition
 	f.pod = &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
@@ -201,6 +220,24 @@ func (f *FBCRegistryPod) podForBundleRegistry() (*corev1.Pod, error) {
 			Namespace: f.cfg.Namespace,
 		},
 		Spec: corev1.PodSpec{
+			Volumes: []corev1.Volume{
+				{
+					Name: k8sutil.TrimDNS1123Label(cm.Name + "-volume"),
+					VolumeSource: corev1.VolumeSource{
+						ConfigMap: &corev1.ConfigMapVolumeSource{
+							Items: []corev1.KeyToPath{
+								{
+									Key:  cm.Name,
+									Path: cm.Name,
+								},
+							},
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: cm.Name,
+							},
+						},
+					},
+				},
+			},
 			Containers: []corev1.Container{
 				{
 					Name:  defaultContainerName,
@@ -212,6 +249,14 @@ func (f *FBCRegistryPod) podForBundleRegistry() (*corev1.Pod, error) {
 					},
 					Ports: []corev1.ContainerPort{
 						{Name: defaultContainerPortName, ContainerPort: f.GRPCPort},
+					},
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name: k8sutil.TrimDNS1123Label(cm.Name + "-volume"),
+							// ReadOnly:  true,
+							MountPath: path.Join(defaultFBCIndexRootDir, cm.Name),
+							SubPath:   cm.Name,
+						},
 					},
 				},
 			},
