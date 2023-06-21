716bfabec62ae0079dae84eaeb1a081cad19878d
Gabe Alford <redhatrises@gmail.com>
2023-06-21 11:26:52 -0600

    fix: update test-docs to fail less randomly (#6464)
    - Seems that the `make test-docs` is failing randomly. This updates
      the config and container version to reduce random failures.
    
    Signed-off-by: Gabe Alford <redhatrises@gmail.com>

--- hack/check-links.sh
+++ hack/check-links.sh
@@ -11,9 +11,10 @@ docker run --rm -v "$(pwd):/src" -v sdk-html:/src/website/public klakegg/hugo:0.
 
 header_text "Checking links"
 # For config explanation: https://github.com/gjtorikian/html-proofer#special-cases-for-the-command-line
-docker run --rm -v sdk-html:/target klakegg/html-proofer:3.18.8 /target \
+docker run --rm -v sdk-html:/target klakegg/html-proofer:3.19.2 /target \
   --empty-alt-ignore \
   --http-status-ignore 429 \
   --allow_hash_href \
-  --typhoeus '{"followlocation":true,"connecttimeout":600,"timeout":600}' \
+  --typhoeus-config='{"ssl_verifypeer":false,"followlocation":true,"connecttimeout":600,"timeout":600}' \
+  --hydra-config='{"max_concurrency":5}' \
   --url-ignore "/github.com\/operator-framework\/operator-sdk\/edit\/master\//,https://docs.github.com/en/get-started/quickstart/fork-a-repo,https://github.com/operator-framework/operator-sdk/settings/access"
